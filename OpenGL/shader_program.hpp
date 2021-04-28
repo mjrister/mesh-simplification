e3be277766aa82ba8038f0a4785eb19706515ce9
Matthew Rister <matthewrister@outlook.com>
2021-04-27 21:14:29 -0700

    feat: add shader validation

--- OpenGL/shader_program.hpp
+++ OpenGL/shader_program.hpp
@@ -9,6 +9,43 @@
 
 class ShaderProgram {
 
+public:
+	ShaderProgram(const std::string_view vertex_shader_filepath, const std::string_view fragment_shader_filepath)
+		: id_{glCreateProgram()} {
+
+		const auto vertex_shader_source = ReadFile(vertex_shader_filepath);
+		const auto fragment_shader_source = ReadFile(fragment_shader_filepath);
+
+		const Shader vertex_shader{vertex_shader_source.c_str(), GL_VERTEX_SHADER};
+		const Shader fragment_shader{fragment_shader_source.c_str(), GL_FRAGMENT_SHADER};
+
+		glAttachShader(id_, vertex_shader.id);
+		glAttachShader(id_, fragment_shader.id);
+
+		glLinkProgram(id_);
+		VerifyStatus(GL_LINK_STATUS);
+
+		glValidateProgram(id_);
+		VerifyStatus(GL_VALIDATE_STATUS);
+
+		glDetachShader(id_, vertex_shader.id);
+		glDetachShader(id_, fragment_shader.id);
+	}
+
+	ShaderProgram(const ShaderProgram&) = delete;
+	ShaderProgram(ShaderProgram&&) noexcept = delete;
+	ShaderProgram& operator=(const ShaderProgram&) = delete;
+	ShaderProgram& operator=(ShaderProgram&&) noexcept = delete;
+
+	~ShaderProgram() {
+		glDeleteProgram(id_);
+	}
+
+	void Enable() const noexcept {
+		glUseProgram(id_);
+	}
+
+private:
 	class Shader {
 
 	public:
@@ -43,40 +80,6 @@ class ShaderProgram {
 		GLuint id{0};
 	};
 
-public:
-	ShaderProgram(const std::string_view vertex_shader_filepath, const std::string_view fragment_shader_filepath)
-		: id_{glCreateProgram()} {
-
-		const auto vertex_shader_source = ReadFile(vertex_shader_filepath);
-		const auto fragment_shader_source = ReadFile(fragment_shader_filepath);
-
-		const Shader vertex_shader{vertex_shader_source.c_str(), GL_VERTEX_SHADER};
-		const Shader fragment_shader{fragment_shader_source.c_str(), GL_FRAGMENT_SHADER};
-
-		glAttachShader(id_, vertex_shader.id);
-		glAttachShader(id_, fragment_shader.id);
-
-		glLinkProgram(id_);
-		VerifyLinkStatus();
-
-		glDetachShader(id_, fragment_shader.id);
-		glDetachShader(id_, vertex_shader.id);
-	}
-
-	ShaderProgram(const ShaderProgram&) = delete;
-	ShaderProgram(ShaderProgram&&) noexcept = delete;
-	ShaderProgram& operator=(const ShaderProgram&) = delete;
-	ShaderProgram& operator=(ShaderProgram&&) noexcept = delete;
-
-	~ShaderProgram() {
-		glDeleteProgram(id_);
-	}
-
-	void Enable() const noexcept {
-		glUseProgram(id_);
-	}
-
-private:
 	static std::string ReadFile(const std::string_view filepath) {
 
 		if (std::ifstream ifs{filepath.data()}; ifs.good()) {
@@ -93,13 +96,13 @@ private:
 		throw std::runtime_error{oss.str()};
 	}
 
-	void VerifyLinkStatus() const {
+	void VerifyStatus(const GLenum type) const {
 		GLint success;
-		glGetProgramiv(id_, GL_LINK_STATUS, &success);
+		glGetProgramiv(id_, type, &success);
 
 		if (!success) {
 			GLsizei log_length{0};
-			glGetShaderiv(id_, GL_INFO_LOG_LENGTH, &log_length);
+			glGetProgramiv(id_, GL_INFO_LOG_LENGTH, &log_length);
 			std::vector<GLchar> info_log(log_length);
 			glGetProgramInfoLog(id_, log_length, &log_length, info_log.data());
 			throw std::runtime_error{info_log.data()};
