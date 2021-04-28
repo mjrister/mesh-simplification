41e1aba9987f84e97e90aa4fa21ea54f0209d1fc
Matthew Rister <matthewrister@outlook.com>
2021-04-27 22:49:03 -0700

    feat: add load mesh interface

--- OpenGL/object_loader.hpp
+++ OpenGL/object_loader.hpp
@@ -1,5 +1,28 @@
 #pragma once
 
+#include <fstream>
+#include <sstream>
+#include <string_view>
+
 class ObjectLoader {
 
+public:
+	static void LoadMesh(const std::string_view object_filepath) {
+
+		if (std::ifstream ifs{object_filepath.data()}; ifs.good()) {
+			LoadMesh(ifs);
+			return;
+		}
+
+		std::ostringstream oss;
+		oss << "Unable to open " << object_filepath;
+		throw std::runtime_error{oss.str()};
+	}
+
+	static void LoadMesh(std::istream& is) {
+
+		for (std::string line; std::getline(is, line);) {
+
+		}
+	}
 };
