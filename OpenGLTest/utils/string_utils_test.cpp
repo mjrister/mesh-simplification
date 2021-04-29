aed1a2996888a1cfc54c81d27181bd4f43ba462e
Matthew Rister <matthewrister@outlook.com>
2021-04-28 21:20:38 -0700

    test: fix broken tests

--- OpenGLTest/utils/string_utils_test.cpp
+++ OpenGLTest/utils/string_utils_test.cpp
@@ -2,48 +2,46 @@
 
 #include "utils/string_utils.hpp"
 
-using namespace utils;
-
 namespace {
 
 	TEST(StringUtils, TestEmptyStringStartsWithPrefix) {
 		constexpr auto* line = "";
 		constexpr auto* prefix = "a";
-		ASSERT_FALSE(StartsWith(line, prefix));
+		ASSERT_FALSE(string::StartsWith(line, prefix));
 	}
 
 	TEST(StringUtils, TestStringStartsWithPrefix) {
 		constexpr auto* line = "Hello, World!";
 		constexpr auto* prefix = "Hello";
-		ASSERT_TRUE(StartsWith(line, prefix));
+		ASSERT_TRUE(string::StartsWith(line, prefix));
 	}
 
 	TEST(StringUtils, TestStringDoesNotStartWithPrefix) {
 		constexpr auto* line = "Hello, World!";
 		constexpr auto* prefix = "World!";
-		ASSERT_TRUE(StartsWith(line, prefix));
+		ASSERT_FALSE(string::StartsWith(line, prefix));
 	}
 
 	TEST(StringUtils, TestShortStringDoesNotContainLongPrefix) {
 		constexpr auto* line = "Hello";
 		constexpr auto* prefix = "Hello, World!";
-		ASSERT_TRUE(StartsWith(line, prefix));
+		ASSERT_FALSE(string::StartsWith(line, prefix));
 	}
 
 	TEST(StringUtils, TestSplitEmptyString) {
 		constexpr auto* delimiter = " ";
-		const auto tokens = Split("", delimiter);
+		const auto tokens = string::Split("", delimiter);
 		ASSERT_TRUE(tokens.empty());
 	}
 
 	TEST(StringUtils, TestSplitWhitespaceString) {
 		constexpr auto* delimiter = " ";
-		const auto tokens = Split("  ", delimiter);
+		const auto tokens = string::Split("  ", delimiter);
 		ASSERT_TRUE(tokens.empty());
 	}
 	TEST(StringUtils, TestSplitStringOnWhitespace) {
 		constexpr auto* delimiter = " ";
-		const auto tokens = Split("   vt 0.707 0.395 0.684   ", delimiter);
+		const auto tokens = string::Split("   vt 0.707 0.395 0.684   ", delimiter);
 		ASSERT_EQ(tokens.size(), 4);
 		ASSERT_EQ(tokens[0], "vt");
 		ASSERT_EQ(tokens[1], "0.707");
