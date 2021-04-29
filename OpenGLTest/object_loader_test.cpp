3b21737246787f43e0333b4e84230f519d96494d
Matthew Rister <matthewrister@outlook.com>
2021-04-28 21:50:33 -0700

    refactor: remove dependency on gtest NuGet package

---
+++ OpenGLTest/object_loader_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "object_loader.hpp"
+
+namespace {
+
+	TEST(ObjectLoader, TestParseLine) {
+		ASSERT_EQ(1, 1);
+	}
+
+	TEST(ObjectLoader, TestParseToken) {
+		ASSERT_TRUE(true);
+	}
+}
