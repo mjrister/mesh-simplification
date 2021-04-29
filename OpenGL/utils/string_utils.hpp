890b69189815d8cec71f00f732aa8eedfb1c342e
Matthew Rister <matthewrister@outlook.com>
2021-04-28 21:49:41 -0700

    fix: use template parameter to avoid conflict with windows macro

--- OpenGL/utils/string_utils.hpp
+++ OpenGL/utils/string_utils.hpp
@@ -13,7 +13,7 @@ namespace string {
 	static std::vector<std::string_view> Split(const std::string_view line, const std::string_view delimiter) {
 		std::vector<std::string_view> tokens;
 		for (auto i = line.find_first_not_of(delimiter); i != std::string_view::npos;) {
-			const auto j = std::min(line.find_first_of(delimiter, i), line.size());
+			const auto j = std::min<std::size_t>(line.find_first_of(delimiter, i), line.size());
 			tokens.push_back(line.substr(i, j - i));
 			i = line.find_first_not_of(delimiter, j);
 		}
