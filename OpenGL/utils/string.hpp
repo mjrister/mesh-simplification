2766cce6b55f4c26f63f205174e114d0ac2fbf3f
Matthew Rister <matthewrister@outlook.com>
2021-05-02 12:15:13 -0700

    refactor: use default string trim parameter

--- OpenGL/utils/string.hpp
+++ OpenGL/utils/string.hpp
@@ -10,7 +10,7 @@ namespace string {
 		return prefix.size() <= string.size() && string.compare(0, prefix.size(), prefix.data()) == 0;
 	}
 
-	static constexpr std::string_view Trim(std::string_view string, const std::string_view delimiter) {
+	static constexpr std::string_view Trim(std::string_view string, const std::string_view delimiter = " \t\r\n") {
 		string.remove_prefix(std::min(string.find_first_not_of(delimiter), string.size()));
 		string.remove_suffix(string.size() - string.find_last_not_of(delimiter) - 1);
 		return string;
@@ -21,7 +21,7 @@ namespace string {
 		if (string.empty()) return {};
 
 		auto i = string.find_first_of(delimiter);
-		if (i == std::string_view::npos) return {string};
+		if (i == std::string_view::npos) return { string };
 
 		i = string.find_first_not_of(delimiter);
 		if (i == std::string_view::npos) return {};
