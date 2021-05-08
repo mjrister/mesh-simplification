97f3c02ce8fead130733c719e71770a85ad2aff4
Matthew Rister <matthewrister@outlook.com>
2021-05-08 10:57:59 -0700

    refactor: add namespace for graphics classes

--- OpenGL/graphics/shader_program.hpp
+++ OpenGL/graphics/shader_program.hpp
@@ -11,62 +11,65 @@
 #include "shader.hpp"
 #include "utils/file.hpp"
 
-class ShaderProgram {
+namespace gfx {
 
-public:
-	ShaderProgram(const std::string_view vertex_shader_filepath, const std::string_view fragment_shader_filepath)
-		: id_{glCreateProgram()},
-		  vertex_shader_{GL_VERTEX_SHADER, file::Read(vertex_shader_filepath).c_str()},
-		  fragment_shader_{GL_FRAGMENT_SHADER, file::Read(fragment_shader_filepath).c_str()} {
+	class ShaderProgram {
 
-		if (!id_) throw std::runtime_error{"Shader program creation failed"};
+	public:
+		ShaderProgram(const std::string_view vertex_shader_filepath, const std::string_view fragment_shader_filepath)
+			: id_{glCreateProgram()},
+			  vertex_shader_{GL_VERTEX_SHADER, file::Read(vertex_shader_filepath).c_str()},
+			  fragment_shader_{GL_FRAGMENT_SHADER, file::Read(fragment_shader_filepath).c_str()} {
 
-		glAttachShader(id_, vertex_shader_.Id());
-		glAttachShader(id_, fragment_shader_.Id());
+			if (!id_) throw std::runtime_error{"Shader program creation failed"};
 
-		glLinkProgram(id_);
-		VerifyStatus(GL_LINK_STATUS);
+			glAttachShader(id_, vertex_shader_.Id());
+			glAttachShader(id_, fragment_shader_.Id());
 
-		glValidateProgram(id_);
-		VerifyStatus(GL_VALIDATE_STATUS);
+			glLinkProgram(id_);
+			VerifyStatus(GL_LINK_STATUS);
 
-		glDetachShader(id_, vertex_shader_.Id());
-		glDetachShader(id_, fragment_shader_.Id());
-	}
+			glValidateProgram(id_);
+			VerifyStatus(GL_VALIDATE_STATUS);
 
-	ShaderProgram(const ShaderProgram&) = delete;
-	ShaderProgram(ShaderProgram&&) noexcept = delete;
-	ShaderProgram& operator=(const ShaderProgram&) = delete;
-	ShaderProgram& operator=(ShaderProgram&&) noexcept = delete;
-
-	~ShaderProgram() {
-		glDeleteProgram(id_);
-	}
-
-	void Enable() const noexcept {
-		glUseProgram(id_);
-	}
-
-	void SetUniform(const std::string_view name, const glm::mat4& value) const {
-		if (const auto location = glGetUniformLocation(id_, name.data()); location != -1) {
-			glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+			glDetachShader(id_, vertex_shader_.Id());
+			glDetachShader(id_, fragment_shader_.Id());
 		}
-	}
 
-private:
-	void VerifyStatus(const GLenum status_type) const {
-		GLint success;
-		glGetProgramiv(id_, status_type, &success);
+		ShaderProgram(const ShaderProgram&) = delete;
+		ShaderProgram(ShaderProgram&&) noexcept = delete;
+		ShaderProgram& operator=(const ShaderProgram&) = delete;
+		ShaderProgram& operator=(ShaderProgram&&) noexcept = delete;
 
-		if (!success) {
-			GLsizei log_length;
-			glGetProgramiv(id_, GL_INFO_LOG_LENGTH, &log_length);
-			std::vector<GLchar> info_log(log_length);
-			glGetProgramInfoLog(id_, log_length, &log_length, info_log.data());
-			throw std::runtime_error{info_log.data()};
+		~ShaderProgram() {
+			glDeleteProgram(id_);
 		}
-	}
 
-	GLuint id_;
-	Shader vertex_shader_, fragment_shader_;
-};
+		void Enable() const noexcept {
+			glUseProgram(id_);
+		}
+
+		void SetUniform(const std::string_view name, const glm::mat4& value) const {
+			if (const auto location = glGetUniformLocation(id_, name.data()); location != -1) {
+				glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+			}
+		}
+
+	private:
+		void VerifyStatus(const GLenum status_type) const {
+			GLint success;
+			glGetProgramiv(id_, status_type, &success);
+
+			if (!success) {
+				GLsizei log_length;
+				glGetProgramiv(id_, GL_INFO_LOG_LENGTH, &log_length);
+				std::vector<GLchar> info_log(log_length);
+				glGetProgramInfoLog(id_, log_length, &log_length, info_log.data());
+				throw std::runtime_error{info_log.data()};
+			}
+		}
+
+		GLuint id_;
+		Shader vertex_shader_, fragment_shader_;
+	};
+}
--- OpenGLTest/graphics/mesh_test.cpp
+++ OpenGLTest/graphics/mesh_test.cpp
@@ -5,49 +5,54 @@
 
 #include "graphics/mesh.hpp"
 
-TEST(Mesh, TestCreateMeshWithNoVertexPositions) {
-	const std::vector<glm::vec3> positions;
-	ASSERT_THROW(Mesh{positions}, std::invalid_argument);
-}
+using namespace gfx;
 
-TEST(Mesh, TestCreateTriangleMeshWithMissingIndices) {
-	const std::vector<glm::vec3> positions{ {0.0f, 0.1f, 0.2f} };
-	ASSERT_THROW((Mesh{positions, {}, {}, {0, 1}}), std::invalid_argument);
-}
+namespace {
 
-TEST(Mesh, TestCreateTriangleMeshWithExtraIndices) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-	ASSERT_THROW((Mesh{positions, {}, {}, {0, 1, 2, 3}}), std::invalid_argument);
-}
+	TEST(Mesh, TestCreateMeshWithNoVertexPositions) {
+		const std::vector<glm::vec3> positions;
+		ASSERT_THROW(Mesh{positions}, std::invalid_argument);
+	}
 
-TEST(Mesh, TestCreateMeshWithMissingTextureCoordinates) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-	const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}};
-	ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
-}
+	TEST(Mesh, TestCreateTriangleMeshWithMissingIndices) {
+		const std::vector<glm::vec3> positions{ {0.0f, 0.1f, 0.2f} };
+		ASSERT_THROW((Mesh{positions, {}, {}, {0, 1}}), std::invalid_argument);
+	}
 
-TEST(Mesh, TestCreateMeshWithExtraTextureCoordinates) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-	const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {0.2f, 0.3f}};
-	ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
-}
+	TEST(Mesh, TestCreateTriangleMeshWithExtraIndices) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
+		ASSERT_THROW((Mesh{positions, {}, {}, {0, 1, 2, 3}}), std::invalid_argument);
+	}
 
-TEST(Mesh, TestCreateMeshWithMissingNormals) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-	const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}};
-	ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
-}
+	TEST(Mesh, TestCreateMeshWithMissingTextureCoordinates) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
+		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}};
+		ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
+	}
 
-TEST(Mesh, TestCreateMeshWithExtraNormals) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-	const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-	ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
-}
+	TEST(Mesh, TestCreateMeshWithExtraTextureCoordinates) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
+		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {0.2f, 0.3f}};
+		ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
+	}
 
-TEST(Mesh, TestCreateMeshWithPositionsTextureCoordinatesNormalsAndIndices) {
-	const std::vector<glm::vec3> positions{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
-	const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {1.0f, 1.1f}, {2.0f, 2.1f}};
-	const std::vector<glm::vec3> normals{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
-	const std::vector<GLuint> indices{0, 1, 2};
-	ASSERT_NO_THROW((Mesh{positions, texture_coordinates, normals, indices}));
+	TEST(Mesh, TestCreateMeshWithMissingNormals) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
+		const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}};
+		ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
+	}
+
+	TEST(Mesh, TestCreateMeshWithExtraNormals) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
+		const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
+		ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
+	}
+
+	TEST(Mesh, TestCreateMeshWithPositionsTextureCoordinatesNormalsAndIndices) {
+		const std::vector<glm::vec3> positions{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
+		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {1.0f, 1.1f}, {2.0f, 2.1f}};
+		const std::vector<glm::vec3> normals{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
+		const std::vector<GLuint> indices{0, 1, 2};
+		ASSERT_NO_THROW((Mesh{positions, texture_coordinates, normals, indices}));
+	}
 }
