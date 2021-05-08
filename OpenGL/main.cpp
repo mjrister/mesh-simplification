d1a76aa66126e47626fe756e2bb3def6c2381217
Matthew Rister <matthewrister@outlook.com>
2021-05-08 00:09:42 -0700

    feat: render Spot

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -6,6 +6,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "graphics/mesh.hpp"
+#include "graphics/object_loader.hpp"
 #include "graphics/shader_program.hpp"
 #include "graphics/texture2d.hpp"
 #include "graphics/window.hpp"
@@ -38,12 +39,12 @@ int main() {
 		const auto width = 640, height = 480;
 		const Window window{"OpenGL", width, height};
 		const ShaderProgram shader_program{"shaders/vertex.glsl", "shaders/fragment.glsl"};
-		const Texture2d texture2d{"resources/textures/container.jpg", GL_TEXTURE0};
+		const Texture2d texture2d{"resources/textures/spot.png", GL_TEXTURE0};
 
 		shader_program.Enable();
 		texture2d.Bind();
 
-		auto container = CreateContainer();
+		auto container = ObjectLoader::LoadMesh("resources/models/spot.obj");
 		container.Initialize();
 
 		const auto projection = glm::perspective(glm::radians(45.0f), static_cast<GLfloat>(width) / height, 0.1f, 100.0f);
@@ -52,7 +53,10 @@ int main() {
 		while (!window.Closed()) {
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			const auto model = glm::rotate(glm::mat4{1.0f}, static_cast<GLfloat>(glfwGetTime()), glm::vec3{0.5f, 1.0f, 0.0f});
+			const auto model = glm::rotate(
+				glm::scale(glm::mat4{1.0f}, glm::vec3{0.5f}),
+				static_cast<GLfloat>(glfwGetTime()),
+				glm::vec3{0.0f, 1.0f, 0.0f});
 			shader_program.SetUniform("model_view_projection", projection * view * model);
 			container.Render();
 			window.SwapBuffers();
