c216a6eaa79558c4185fdabe346b7462d68af87e
Matthew Rister <matthewrister@outlook.com>
2021-05-08 00:59:07 -0700

    test: fix object loader tests

--- OpenGLTest/graphics/object_loader_test.cpp
+++ OpenGLTest/graphics/object_loader_test.cpp
@@ -27,11 +27,6 @@ protected:
 		ObjectLoader::ValidateIndex(index, max_value);
 	}
 
-	template <typename T, std::uint8_t N>
-	static auto Flatten(const std::vector<glm::vec<N, T>>& tuples) {
-		return ObjectLoader::Flatten(tuples);
-	}
-
 	static constexpr auto npos = ObjectLoader::npos_index_;
 };
 
@@ -71,19 +66,19 @@ namespace {
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseIndexGroupWithPositionIndex) {
-		ASSERT_EQ((glm::ivec3{1, npos, npos}), ParseIndexGroup("1"));
+		ASSERT_EQ((glm::ivec3{0, npos, npos}), ParseIndexGroup("1"));
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseIndexGroupWithPositionAndTextureCoordinatesIndices) {
-		ASSERT_EQ((glm::ivec3{1, 2, npos}), ParseIndexGroup("1/2"));
+		ASSERT_EQ((glm::ivec3{0, 1, npos}), ParseIndexGroup("1/2"));
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseIndexGroupWithPositionAndNormalIndices) {
-		ASSERT_EQ((glm::ivec3{1, npos, 2}), ParseIndexGroup("1//2"));
+		ASSERT_EQ((glm::ivec3{0, npos, 1}), ParseIndexGroup("1//2"));
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseIndexGroupWithPositionTextureCoordinateAndNormalIndices) {
-		ASSERT_EQ((glm::ivec3{1, 2, 3}), ParseIndexGroup("1/2/3"));
+		ASSERT_EQ((glm::ivec3{0, 1, 2}), ParseIndexGroup("1/2/3"));
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseIndexGroupWithInvalidFormat) {
@@ -102,11 +97,11 @@ namespace {
 			glm::ivec3{0, 1, 2},
 			glm::ivec3{3, 4, 5},
 			glm::ivec3{6, 7, 8}
-		}), ParseFace("f 0/1/2 3/4/5 6/7/8"));
+		}), ParseFace("f 1/2/3 4/5/6 7/8/9"));
 	}
 
 	TEST_F(ObjectLoaderTest, TestParseFaceWithIncorrectNumberOfIndexGroups) {
-		ASSERT_THROW(ParseFace("f 0/1/2 3/4/5"), std::invalid_argument);
+		ASSERT_THROW(ParseFace("f 1/2/3 4/5/6"), std::invalid_argument);
 	}
 
 	TEST_F(ObjectLoaderTest, TestValidateIndex) {
@@ -116,26 +111,6 @@ namespace {
 		ASSERT_NO_THROW(ValidateIndex(max_value, max_value));
 		ASSERT_THROW(ValidateIndex(min_value - 1, max_value), std::invalid_argument);
 		ASSERT_THROW(ValidateIndex(max_value + 1, max_value), std::invalid_argument);
-	}
-
-	TEST_F(ObjectLoaderTest, TestFlattenVec3) {
-		const std::vector expected{0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f};
-		const auto actual = Flatten(std::vector<glm::vec3>{
-			{0.f, 1.f, 2.f},
-			{3.f, 4.f, 5.f},
-			{6.f, 7.f, 8.f}
-		});
-		ASSERT_EQ(expected, actual);
-	}
-
-	TEST_F(ObjectLoaderTest, TestFlattenVec2) {
-		const std::vector expected{0.f, 1.f, 2.f, 3.f, 4.f, 5.f};
-		const auto actual = Flatten(std::vector<glm::vec2>{
-			{0.f, 1.f},
-			{2.f, 3.f},
-			{4.f, 5.f}
-		});
-		ASSERT_EQ(expected, actual);
 	}
 
 	TEST_F(ObjectLoaderTest, TestLoadMesh) {
@@ -160,21 +135,21 @@ namespace {
 			vn 1.0 1.1 1.2
 			vn 2.0 2.1 2.2
 			# faces
-			f 0/0/2 1/1/1 5/3/0
-			f 1/1/1 2/2/0 3/4/1
-			f 1/1/1 3/4/1 5/3/0
-			f 5/3/0 3/4/1 4/5/2
+			f 1/1/3 2/2/2 6/4/1
+			f 2/2/2 3/3/1 4/5/2
+			f 2/2/2 4/5/2 6/4/1
+			f 6/4/1 4/5/2 5/6/3
 		)"};
 
 		const auto mesh = ObjectLoader::LoadMesh(ss);
 
-		ASSERT_EQ((std::vector{
-			0.0f,  1.0f, 0.0f,
-			0.5f,  1.0f, 0.0f,
-			1.0f,  1.0f, 0.0f,
-			0.75f, 0.5f, 0.0f,
-			0.5f,  0.0f, 0.0f,
-			0.25f, 0.5f, 0.0f
+		ASSERT_EQ((std::vector<glm::vec3>{
+			{0.0f,  1.0f, 0.0f},
+			{0.5f,  1.0f, 0.0f},
+			{1.0f,  1.0f, 0.0f},
+			{0.75f, 0.5f, 0.0f},
+			{0.5f,  0.0f, 0.0f},
+			{0.25f, 0.5f, 0.0f}
 		}), mesh.Positions());
 
 		ASSERT_EQ((std::vector{
@@ -184,22 +159,22 @@ namespace {
 			5u, 3u, 4u
 		}), mesh.Indices());
 
-		ASSERT_EQ((std::vector{
-			0.0f,  0.0f,
-			0.5f,  0.0f,
-			1.0f,  0.0f,
-			0.75f, 0.5f,
-			0.5f,  1.0f,
-			0.25f, 0.5f
+		ASSERT_EQ((std::vector<glm::vec2>{
+			{0.0f,  0.0f},
+			{0.5f,  0.0f},
+			{1.0f,  0.0f},
+			{0.75f, 0.5f},
+			{0.5f,  1.0f},
+			{0.25f, 0.5f}
 		}), mesh.TextureCoordinates());
 
-		ASSERT_EQ((std::vector{
-			2.0f, 2.1f, 2.2f,
-			1.0f, 1.1f, 1.2f,
-			0.0f, 0.1f, 0.2f,
-			1.0f, 1.1f, 1.2f,
-			2.0f, 2.1f, 2.2f,
-			0.0f, 0.1f, 0.2f
+		ASSERT_EQ((std::vector<glm::vec3>{
+			{2.0f, 2.1f, 2.2f},
+			{1.0f, 1.1f, 1.2f},
+			{0.0f, 0.1f, 0.2f},
+			{1.0f, 1.1f, 1.2f},
+			{2.0f, 2.1f, 2.2f},
+			{0.0f, 0.1f, 0.2f}
 		}), mesh.Normals());
 	}
 }
