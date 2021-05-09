eb4bf53ef58f1c750308295ad9b783640d17212f
Matthew Rister <matthewrister@outlook.com>
2021-05-08 18:59:44 -0700

    refactor: remove debug string

--- OpenGL/graphics/window.hpp
+++ OpenGL/graphics/window.hpp
@@ -72,10 +72,6 @@ namespace gfx {
 			std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl
 				<< "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
-			GLint max_vertex_attributes;
-			glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &max_vertex_attributes);
-			std::cout << "Maximum vertex attributes supported: " << max_vertex_attributes << std::endl;
-
 			glEnable(GL_DEBUG_OUTPUT);
 			glDebugMessageCallback(PrintDebugErrorMessage, nullptr);
 	#endif
