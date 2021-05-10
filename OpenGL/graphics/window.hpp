99f7f725fbc05bd32d05af3cadd057eb9208ce8c
Matthew Rister <matthewrister@outlook.com>
2021-05-10 01:30:11 -0700

    refactor: declare opengl version in main

--- OpenGL/graphics/window.hpp
+++ OpenGL/graphics/window.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string_view>
+#include <utility>
 
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
@@ -13,10 +14,14 @@ namespace gfx {
 	class Window {
 
 	public:
-		Window(const std::string_view title, const int32_t width, const int32_t height) {
+		Window(const std::string_view title,
+		       const std::pair<std::int32_t, std::int32_t>& width_and_height,
+		       const std::pair<std::int32_t, std::int32_t>& opengl_major_and_minor_version) {
 
-			InitializeGlfw();
+			const auto [opengl_major_version, opengl_minor_version] = opengl_major_and_minor_version;
+			InitializeGlfw(opengl_major_version, opengl_minor_version);
 
+			const auto [width, height] = width_and_height;
 			window_ = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
 			if (!window_) throw std::runtime_error{"Window creation failed"};
 
@@ -24,7 +29,7 @@ namespace gfx {
 			glfwSetFramebufferSizeCallback(window_, OnWindowResize);
 			glfwSetKeyCallback(window_, OnKeyPress);
 
-			InitializeGl3w();
+			InitializeGl3w(opengl_major_version, opengl_minor_version);
 			glEnable(GL_DEPTH_TEST);
 		}
 
@@ -47,22 +52,22 @@ namespace gfx {
 		}
 
 	private:
-		static void InitializeGlfw() {
+		static void InitializeGlfw(const std::int32_t opengl_major_version, const std::int32_t opengl_minor_version) {
 
 			if (!glfwInit()) throw std::runtime_error{"GLFW initialization failed"};
 
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, opengl_major_version_);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, opengl_minor_version_);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, opengl_major_version);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, opengl_minor_version);
 			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		}
 
-		static void InitializeGl3w() {
+		static void InitializeGl3w(const std::int32_t opengl_major_version, const std::int32_t opengl_minor_version) {
 
 			if (gl3wInit()) throw std::runtime_error{"OpenGL initialization failed"};
 
-			if (!gl3wIsSupported(opengl_major_version_, opengl_minor_version_)) {
+			if (!gl3wIsSupported(opengl_major_version, opengl_minor_version)) {
 				std::ostringstream oss;
-				oss << "OpenGL " << opengl_major_version_ << "." << opengl_minor_version_ << "not supported";
+				oss << "OpenGL " << opengl_major_version << "." << opengl_minor_version << "not supported";
 				throw std::runtime_error{oss.str()};
 			}
 
@@ -108,7 +113,6 @@ namespace gfx {
 			}
 		}
 
-		static constexpr int32_t opengl_major_version_{4}, opengl_minor_version_{6};
 		GLFWwindow* window_{};
 	};
 }
--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -16,8 +16,9 @@
 int main() {
 
 	try {
-		constexpr std::int32_t width = 1280, height = 960;
-		const gfx::Window window{"OpenGL", width, height};
+		constexpr std::int32_t width{1280}, height{960};
+		constexpr std::int32_t major_version{4}, minor_version{6};
+		const gfx::Window window{"OpenGL", std::make_pair(width, height), std::make_pair(major_version, minor_version)};
 
 		const gfx::ShaderProgram shader_program{"shaders/vertex.glsl", "shaders/fragment.glsl"};
 		shader_program.Enable();
