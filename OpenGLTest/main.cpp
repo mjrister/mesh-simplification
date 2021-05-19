ce6c21b8f5bbf6d3e763755c67d8a76def8cac5a
Matthew Rister <matthewrister@outlook.com>
2021-05-19 00:20:52 -0700

    test: initialize glfw and gl3w so opengl calls don't fail

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -1,6 +1,41 @@
 #include <gtest/gtest.h>
 
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+namespace {
+	void InitializeGlfw() {
+		if (!glfwInit()) throw std::runtime_error{"GLFW initialization failed"};
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+	}
+
+	GLFWwindow* CreateGlfwWindow() {
+		if (const auto window = glfwCreateWindow(100, 100, "Test", nullptr, nullptr)) {
+			glfwMakeContextCurrent(window);
+			return window;
+		}
+		throw std::runtime_error{"Window creation failed"};
+	}
+
+	void InitializeGl3w() {
+		if (gl3wInit()) {
+			throw std::runtime_error{"OpenGL initialization failed"};
+		}
+	}
+}
+
 int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	try {
+		InitializeGlfw();
+		auto* const window = CreateGlfwWindow();
+		InitializeGl3w();
+		testing::InitGoogleTest(&argc, argv);
+		const auto exit_code = RUN_ALL_TESTS();
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return exit_code;
+	} catch (const std::exception& e) {
+		std::cerr << e.what();
+		return EXIT_FAILURE;
+	}
 }
