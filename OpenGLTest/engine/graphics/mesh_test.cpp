1c22edcfd116ccc46253543b23455fce0752f8c0
Matthew Rister <matthewrister@outlook.com>
2021-05-20 22:37:30 -0700

    test: verify private methods in anonymous namespace

--- OpenGLTest/engine/graphics/mesh_test.cpp
+++ OpenGLTest/engine/graphics/mesh_test.cpp
@@ -3,53 +3,35 @@
 #include <glm/glm.hpp>
 #include <gtest/gtest.h>
 
-#include "engine/graphics/mesh.h"
+#include "engine/graphics/mesh.cpp"
 
 namespace {
 	using namespace gfx;
 
-	TEST(Mesh, TestCreateMeshWithNoVertexPositions) {
+	TEST(Mesh, TestValidateMeshWithNoPositions) {
 		const std::vector<glm::vec3> positions;
-		ASSERT_THROW(Mesh{positions}, std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {}, {}, {}), std::invalid_argument);
 	}
 
-	TEST(Mesh, TestCreateTriangleMeshWithMissingIndices) {
+	TEST(Mesh, TestValidateMeshWithInvalidIndices) {
 		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-		const std::vector<GLuint> indices{0, 1};
-		ASSERT_THROW((Mesh{positions, {}, {}, indices}), std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {}, {}, {0, 1}), std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {}, {}, {0, 1, 2, 3}), std::invalid_argument);
 	}
 
-	TEST(Mesh, TestCreateTriangleMeshWithExtraIndices) {
-		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-		const std::vector<GLuint> indices{0, 1, 2, 3};
-		ASSERT_THROW((Mesh{positions, {}, {}, indices}), std::invalid_argument);
-	}
-
-	TEST(Mesh, TestCreateMeshWithMissingTextureCoordinates) {
+	TEST(Mesh, TestValidateMeshWithInvalidTextureCoordinates) {
 		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}};
-		ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {{0.0f, 0.1f}}, {}, {}), std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {{0.0f, 0.1f}, {0.2f, 0.3f}, {0.4f, 0.5f}}, {}, {}), std::invalid_argument);
 	}
 
-	TEST(Mesh, TestCreateMeshWithExtraTextureCoordinates) {
-		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {0.2f, 0.3f}};
-		ASSERT_THROW((Mesh{positions, texture_coordinates}), std::invalid_argument);
-	}
-
-	TEST(Mesh, TestCreateMeshWithMissingNormals) {
+	TEST(Mesh, TestValidateMeshInvalidNormals) {
 		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-		const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}};
-		ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
+		ASSERT_THROW(Validate(positions, {}, {{0.0f, 0.1f, 0.2f}}, {}), std::invalid_argument);
+		ASSERT_THROW((Mesh{positions, {}, {{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0f, 2.1f, 2.2f}}}), std::invalid_argument);
 	}
 
-	TEST(Mesh, TestCreateMeshWithExtraNormals) {
-		const std::vector<glm::vec3> positions{{0.0f, 0.1f, 0.2f}};
-		const std::vector<glm::vec3> normals{{0.0f, 0.1f, 0.2f}, {1.0f, 1.1f, 1.2f}};
-		ASSERT_THROW((Mesh{positions, {}, normals}), std::invalid_argument);
-	}
-
-	TEST(Mesh, TestCreateMeshWithPositionsTextureCoordinatesNormalsAndIndices) {
+	TEST(Mesh, TestValidateMeshWithPositionsTextureCoordinatesNormalsAndIndices) {
 		const std::vector<glm::vec3> positions{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
 		const std::vector<glm::vec2> texture_coordinates{{0.0f, 0.1f}, {1.0f, 1.1f}, {2.0f, 2.1f}};
 		const std::vector<glm::vec3> normals{{0.0f, 0.1, 0.2f}, {1.0f, 1.1f, 1.2f}, {2.0, 2.1f, 2.2f}};
--- OpenGLTest/engine/graphics/obj_loader_test.cpp
+++ OpenGLTest/engine/graphics/obj_loader_test.cpp
@@ -1,187 +1,137 @@
 #include <glm/glm.hpp>
 #include <gtest/gtest.h>
 
-#include "engine/graphics/mesh.h"
-#include "engine/graphics/obj_loader.h"
+#include "engine/graphics/obj_loader.cpp"
 
 namespace {
 	using namespace gfx;
 
-	void AssertLoadMeshThrowsWithInvalidArgument(const std::string_view obj_source) {
-		std::istringstream ss{obj_source.data()};
-		ASSERT_THROW(obj_loader::LoadMesh(ss), std::invalid_argument);
+	TEST(ObjLoaderTest, TestParseEmptyToken) {
+		ASSERT_THROW(ParseToken<GLint>(""), std::invalid_argument);
 	}
 
-	TEST(ObjLoaderTest, TestLoadEmptyMeshThrowsAnException) {
-		AssertLoadMeshThrowsWithInvalidArgument("");
+	TEST(ObjLoaderTest, TestParseInvalidToken) {
+		ASSERT_THROW(ParseToken<GLfloat>("Definitely a float"), std::invalid_argument);
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithInvalidPosition) {
-		AssertLoadMeshThrowsWithInvalidArgument("v ");
-		AssertLoadMeshThrowsWithInvalidArgument("v 0.0 0.1");
-		AssertLoadMeshThrowsWithInvalidArgument("v 0.0 0.1 0.2 0.3");
-		AssertLoadMeshThrowsWithInvalidArgument("v a b c");
+	TEST(ObjLoaderTest, TestParseIntToken) {
+		ASSERT_EQ(42, ParseToken<GLint>("42"));
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithInvalidTextureCoordinate) {
-		AssertLoadMeshThrowsWithInvalidArgument("vt ");
-		AssertLoadMeshThrowsWithInvalidArgument("vt 0.0");
-		AssertLoadMeshThrowsWithInvalidArgument("vt 0.0 0.1 0.2");
-		AssertLoadMeshThrowsWithInvalidArgument("vt a b");
+	TEST(ObjLoaderTest, TestParseFloatToken) {
+		ASSERT_FLOAT_EQ(3.14f, ParseToken<GLfloat>("3.14"));
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithInvalidNormal) {
-		AssertLoadMeshThrowsWithInvalidArgument("vn ");
-		AssertLoadMeshThrowsWithInvalidArgument("vn 0.0 0.1");
-		AssertLoadMeshThrowsWithInvalidArgument("vn 0.0 0.1 0.2 0.3");
-		AssertLoadMeshThrowsWithInvalidArgument("vn a b c");
+	TEST(ObjLoaderTest, TestParseEmptyLine) {
+		ASSERT_THROW((ParseLine<GLfloat, 3>("")), std::invalid_argument);
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithInvalidFace) {
-		AssertLoadMeshThrowsWithInvalidArgument("f ");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0 1");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0 1 2 3");
-		AssertLoadMeshThrowsWithInvalidArgument("f a b c");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0/a 1/b 1/c");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0//a 1//b 2//c");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0/1/a 2/3/b 4/5/c");
-		AssertLoadMeshThrowsWithInvalidArgument("f / / /");
-		AssertLoadMeshThrowsWithInvalidArgument("f // // //");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0/ 0/ 0/");
-		AssertLoadMeshThrowsWithInvalidArgument("f /1 /1 /1");
-		AssertLoadMeshThrowsWithInvalidArgument("f 0/1/ 0/1/ 0/1/");
-		AssertLoadMeshThrowsWithInvalidArgument("f /1/2 /1/2 /1/2");
+	TEST(ObjLoaderTest, TestParseLineWithInvalidSizeArgument) {
+		ASSERT_THROW((ParseLine<GLfloat, 2>("vt 0.707 0.395 0.684")), std::invalid_argument);
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithPositions) {
+	TEST(ObjLoaderTest, TestParseLine) {
+		ASSERT_EQ((glm::vec3{0.707f, 0.395f, 0.684f}), (ParseLine<GLfloat, 3>("vt 0.707 0.395 0.684")));
+	}
+
+	TEST(ObjLoaderTest, TestParseIndexGroupWithPositionIndex) {
+		ASSERT_EQ((glm::ivec3{0, npos_index, npos_index}), ParseIndexGroup("1"));
+	}
+
+	TEST(ObjLoaderTest, TestParseIndexGroupWithPositionAndTextureCoordinatesIndices) {
+		ASSERT_EQ((glm::ivec3{0, 1, npos_index}), ParseIndexGroup("1/2"));
+	}
+
+	TEST(ObjLoaderTest, TestParseIndexGroupWithPositionAndNormalIndices) {
+		ASSERT_EQ((glm::ivec3{0, npos_index, 1}), ParseIndexGroup("1//2"));
+	}
+
+	TEST(ObjLoaderTest, TestParseIndexGroupWithPositionTextureCoordinateAndNormalIndices) {
+		ASSERT_EQ((glm::ivec3{0, 1, 2}), ParseIndexGroup("1/2/3"));
+	}
+
+	TEST(ObjLoaderTest, TestParseInvalidIndexGroup) {
+		ASSERT_THROW(ParseIndexGroup(""), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("/"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("//"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("1/"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("/2"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("1//"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("/2/"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("//3"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("1/2/"), std::invalid_argument);
+		ASSERT_THROW(ParseIndexGroup("/2/3"), std::invalid_argument);
+	}
+
+	TEST(ObjLoaderTest, TestParseFaceWithInvalidNumberOfIndexGroups) {
+		ASSERT_THROW(ParseFace("f 1/2/3 4/5/6"), std::invalid_argument);
+		ASSERT_THROW(ParseFace("f 1/2/3 4/5/6 7/8/9 10/11/12"), std::invalid_argument);
+	}
+
+	TEST(ObjLoaderTest, TestParseFaceWithThreeIndexGroups) {
+		ASSERT_EQ((std::array<glm::ivec3, 3>{
+			glm::ivec3{0, 1, 2},
+			glm::ivec3{3, 4, 5},
+			glm::ivec3{6, 7, 8}
+		}), ParseFace("f 1/2/3 4/5/6 7/8/9"));
+	}
+
+	TEST(ObjLoaderTest, TestLoadMeshWithoutFaceIndices) {
+
 		std::istringstream ss{R"(
+			# positions
 			v 0.0 0.1 0.2
 			v 1.0 1.1 1.2
 			v 2.0 2.1 2.2
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f};
-		ASSERT_EQ((std::vector{v0, v1, v2}), mesh.Positions());
-	}
-
-	TEST(ObjLoaderTest, TestLoadMeshWithTextureCoordinates) {
-		std::istringstream ss{R"(
-			v 0.0 0.1 0.2
-			v 1.0 1.1 1.2
-			v 2.0 2.1 2.2
+			# texture coordinates
 			vt 3.0 3.1
 			vt 4.0 4.1
 			vt 5.0 5.1
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		constexpr glm::vec2 vt0{3.0f, 3.1f}, vt1{4.0f, 4.1f}, vt2{5.0f, 5.1f};
-		ASSERT_EQ((std::vector{vt0, vt1, vt2}), mesh.TextureCoordinates());
-	}
-
-	TEST(ObjLoaderTest, TestLoadMeshWithNormals) {
-		std::istringstream ss{R"(
-			v 0.0 0.1 0.2
-			v 1.0 1.1 1.2
-			v 2.0 2.1 2.2
+			# normals
 			vn 6.0 6.1 6.2
 			vn 7.0 7.1 7.2
 			vn 8.0 8.1 8.2
 		)"};
+
 		const auto mesh = obj_loader::LoadMesh(ss);
+		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f};
+		constexpr glm::vec2 vt0{3.0f, 3.1f}, vt1{4.0f, 4.1f}, vt2{5.0f, 5.1f};
 		constexpr glm::vec3 vn0{6.0f, 6.1f, 6.2f}, vn1{7.0f, 7.1f, 7.2f}, vn2{8.0f, 8.1f, 8.2f};
+
+		ASSERT_EQ((std::vector{v0, v1, v2}), mesh.Positions());
+		ASSERT_EQ((std::vector{vt0, vt1, vt2}), mesh.TextureCoordinates());
 		ASSERT_EQ((std::vector{vn0, vn1, vn2}), mesh.Normals());
 	}
 
-	TEST(ObjLoaderTest, TestLoadMeshWithPositionIndices) {
-		std::istringstream ss{R"(
-			v 0.0 0.1 0.2
-			v 1.0 1.1 1.2
-			v 2.0 2.1 2.2
-			v 3.0 3.1 3.2
-			f 1 2 3
-			f 1 2 4
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f}, v3{3.0f, 3.1f, 3.2f};
-		ASSERT_EQ((std::vector{v0, v1, v2, v0, v1, v3}), mesh.Positions());
-	}
+	TEST(ObjLoaderTest, TestLoadMeshWithFaceIndices) {
 
-	TEST(ObjLoaderTest, TestLoadMeshWithPositionAndTextureCoordinateIndices) {
 		std::istringstream ss{R"(
+			# positions
 			v 0.0 0.1 0.2
 			v 1.0 1.1 1.2
 			v 2.0 2.1 2.2
 			v 3.0 3.1 3.2
+			# texture coordinates
 			vt 4.0 4.1
 			vt 5.0 5.1
 			vt 6.0 6.1
 			vt 7.0 7.1
-			f 1/1 2/2 3/4
-			f 1/1 2/2 4/3
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f}, v3{3.0f, 3.1f, 3.2f};
-		constexpr glm::vec2 vt0{4.0f, 4.1f}, vt1{5.0f, 5.1f}, vt2{6.0f, 6.1f}, vt3{7.0f, 7.1f};
-		ASSERT_EQ((std::vector{v0, v1, v2, v0, v1, v3}), mesh.Positions());
-		ASSERT_EQ((std::vector{vt0, vt1, vt3, vt0, vt1, vt2}), mesh.TextureCoordinates());
-	}
-
-	TEST(ObjLoaderTest, TestLoadMeshWithPositionAndNormalIndices) {
-		std::istringstream ss{R"(
-			v 0.0 0.1 0.2
-			v 1.0 1.1 1.2
-			v 2.0 2.1 2.2
-			v 3.0 3.1 3.2
+			# normals
 			vn 8.0  8.1  8.2
 			vn 9.0  9.1  9.2
 			vn 10.0 10.1 10.2
-			f 1//1 2//2 3//3
-			f 1//3 2//2 4//1
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f}, v3{3.0f, 3.1f, 3.2f};
-		constexpr glm::vec3 vn0{8.0f, 8.1f, 8.2f}, vn1{9.0f, 9.1f, 9.2f}, vn2{10.0f, 10.1f, 10.2f};
-		ASSERT_EQ((std::vector{v0, v1, v2, v0, v1, v3}), mesh.Positions());
-		ASSERT_EQ((std::vector{vn0, vn1, vn2, vn2, vn1, vn0}), mesh.Normals());
-	}
-
-	TEST(ObjLoaderTest, TestLoadMeshWithPositionTextureCoordinatesAndNormalIndices) {
-		std::istringstream ss{R"(
-			v 0.0 0.1 0.2
-			v 1.0 1.1 1.2
-			v 2.0 2.1 2.2
-			v 3.0 3.1 3.2
-			vt 4.0 4.1
-			vt 5.0 5.1
-			vt 6.0 6.1
-			vt 7.0 7.1
-			vn 8.0  8.1  8.2
-			vn 9.0  9.1  9.2
-			vn 10.0 10.1 10.2
+			# faces
 			f 1/1/1 2/2/2 3/4/3
 			f 1/1/3 2/2/2 4/3/1
 		)"};
+
 		const auto mesh = obj_loader::LoadMesh(ss);
 		constexpr glm::vec3 v0{0.0f, 0.1f, 0.2f}, v1{1.0f, 1.1f, 1.2f}, v2{2.0f, 2.1f, 2.2f}, v3{3.0f, 3.1f, 3.2f};
 		constexpr glm::vec2 vt0{4.0f, 4.1f}, vt1{5.0f, 5.1f}, vt2{6.0f, 6.1f}, vt3{7.0f, 7.1f};
 		constexpr glm::vec3 vn0{8.0f, 8.1f, 8.2f}, vn1{9.0f, 9.1f, 9.2f}, vn2{10.0f, 10.1f, 10.2f};
+
 		ASSERT_EQ((std::vector{v0, v1, v2, v0, v1, v3}), mesh.Positions());
 		ASSERT_EQ((std::vector{vt0, vt1, vt3, vt0, vt1, vt2}), mesh.TextureCoordinates());
 		ASSERT_EQ((std::vector{vn0, vn1, vn2, vn2, vn1, vn0}), mesh.Normals());
-	}
-
-	TEST(ObjLoaderTest, TestLoadMeshWithComments) {
-		std::istringstream ss{R"(
-			# position
-			v 0.0 0.1 0.2
-			# texture coordinates
-			# vt 1.1 1.0
-			# normals
-			vn 2.0 2.1 2.2
-		)"};
-		const auto mesh = obj_loader::LoadMesh(ss);
-		ASSERT_EQ((std::vector<glm::vec3>{{0.0f, 0.1f, 0.2f}}), mesh.Positions());
-		ASSERT_EQ((std::vector<glm::vec2>{}), mesh.TextureCoordinates());
-		ASSERT_EQ((std::vector<glm::vec3>{{2.0f, 2.1f, 2.2f}}), mesh.Normals());
 	}
 }
