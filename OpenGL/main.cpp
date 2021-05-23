d0f566bf33e2186b8c62e874c4a001a7aecce03e
Matthew Rister <matthewrister@outlook.com>
2021-05-23 00:37:19 -0700

    refactor: tweak light settings

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -115,8 +115,8 @@ int main() {
 			shader_program.SetUniform("view_model_transform", view_model_transform);
 			shader_program.SetUniform("normal_transform", glm::mat3{view_model_transform});
 
-			point_light_angle += delta_time;
-			const glm::vec4 point_light_position{std::cos(point_light_angle), std::sin(point_light_angle), 0.f, 1.0f};
+			point_light_angle += 0.5f * delta_time;
+			const glm::vec4 point_light_position{std::cos(point_light_angle), std::sin(point_light_angle), 1.f, 1.0f};
 			shader_program.SetUniform("point_light.position", view_transform * point_light_position);
 
 			HandleInput(window, view_model_transform, mesh);
