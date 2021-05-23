a6379e203937203f28015c364c22e81a3f7a9fc7
Matthew Rister <matthewrister@outlook.com>
2021-05-23 10:31:06 -0700

    refactor: improve performance using swizzling to convert to vec3

--- OpenGL/shaders/fragment.glsl
+++ OpenGL/shaders/fragment.glsl
@@ -24,18 +24,22 @@ out vec4 fragment_color;
 
 void main() {
 	vec3 light_color = material.ambient;
-	vec3 light_direction = vec3(point_light.position - vertex.position);
+
+	vec3 light_direction = (point_light.position - vertex.position).xyz;
 	float light_distance = length(light_direction);
 	light_direction = normalize(light_direction);
+
 	vec3 normal = normalize(vertex.normal);
 	float diffuse_intensity = max(dot(light_direction, normal), 0.f);
 
 	if (diffuse_intensity > 0.f) {
 		vec3 diffuse_color = diffuse_intensity * material.diffuse;
+
 		vec3 reflect_direction = normalize(reflect(-light_direction, normal));
 		vec3 view_direction = normalize(-vertex.position.xyz);
 		float specular_intensity = pow(max(dot(reflect_direction, view_direction), 0.f), material.shininess);
 		vec3 specular_color = specular_intensity * material.specular;
+
 		float attenuation = dot(point_light.attenuation, vec3(1.f, light_distance, light_distance * light_distance));
 		light_color += point_light.color * point_light.intensity * (diffuse_color + specular_color) / attenuation;
 	}
