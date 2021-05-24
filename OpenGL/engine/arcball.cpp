4dea6b93adc46ebc2f390184febb872e838a25f4
Matthew Rister <matthewrister@outlook.com>
2021-05-23 22:19:03 -0700

    test: add get arcball position tests

--- OpenGL/engine/arcball.cpp
+++ OpenGL/engine/arcball.cpp
@@ -8,7 +8,7 @@
 #include "window.h"
 
 namespace {
-	glm::vec2 GetNormalizedCursorPosition(
+	constexpr glm::vec2 GetNormalizedCursorPosition(
 		const glm::dvec2& cursor_position, const std::int32_t window_width, const std::int32_t window_height) {
 
 		const auto x_norm = static_cast<GLfloat>(cursor_position.x * 2.0 / window_width - 1.0);
@@ -17,10 +17,7 @@ namespace {
 		return {std::clamp(x_norm, -1.f, 1.f), std::clamp(-y_norm, -1.f, 1.f)};
 	}
 
-	glm::vec3 GetArcballPosition(
-		const glm::dvec2& cursor_position, const std::int32_t window_width, const std::int32_t window_height) {
-
-		const auto cursor_position_norm = GetNormalizedCursorPosition(cursor_position, window_width, window_height);
+	glm::vec3 GetArcballPosition(const glm::vec2& cursor_position_norm) {
 		const auto x = cursor_position_norm.x;
 		const auto y = cursor_position_norm.y;
 
@@ -36,8 +33,11 @@ std::optional<const std::pair<const glm::vec3, const GLfloat>> arcball::GetRotat
 	const Window& window, const glm::dvec2& cursor_position, const glm::dvec2& prev_cursor_position) {
 
 	const auto [width, height] = window.Size();
-	const auto arcball_position = GetArcballPosition(cursor_position, width, height);
-	const auto prev_arcball_position = GetArcballPosition(prev_cursor_position, width, height);
+	const auto cursor_position_norm = GetNormalizedCursorPosition(cursor_position, width, height);
+	const auto prev_cursor_position_norm = GetNormalizedCursorPosition(prev_cursor_position, width, height);
+
+	const auto arcball_position = GetArcballPosition(cursor_position_norm);
+	const auto prev_arcball_position = GetArcballPosition(prev_cursor_position_norm);
 	const auto angle = std::acos(std::min<>(1.f, glm::dot(prev_arcball_position, arcball_position)));
 
 	if (static constexpr GLfloat epsilon = 1e-3f; angle > epsilon) {
--- OpenGLTest/arcball_test.cpp
+++ OpenGLTest/arcball_test.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <cmath>
+#include <limits>
+
+#include <glm/gtc/epsilon.hpp>
+
 #include "engine/arcball.cpp"
 
 namespace {
 
 	TEST(ArcballTest, TestGetNormalizedCursorPosition) {
-		constexpr auto width{300}, height{200};
+		constexpr auto width = 300, height = 200;
 
 		auto cursor_position_norm = GetNormalizedCursorPosition(glm::dvec2{0.f, 0.f}, width, height);
 		ASSERT_FLOAT_EQ(cursor_position_norm.x, -1.f);
@@ -34,5 +39,22 @@ namespace {
 		cursor_position_norm = GetNormalizedCursorPosition(glm::dvec2{width + 1.f, height + 1.f}, width, height);
 		ASSERT_FLOAT_EQ(cursor_position_norm.x, 1.f);
 		ASSERT_FLOAT_EQ(cursor_position_norm.y, -1.f);
+	}
+
+	TEST(ArcballTest, TestGetArcballPositionForCursorInsideSphere) {
+		constexpr auto x = 0.5f, y = .25f;
+		constexpr glm::vec2 cursor_position_ndc{x, y};
+		const auto arcball_position = GetArcballPosition(cursor_position_ndc);
+		ASSERT_FLOAT_EQ(arcball_position.x, x);
+		ASSERT_FLOAT_EQ(arcball_position.y, y);
+		ASSERT_FLOAT_EQ(arcball_position.z, .82915622f);
+	}
+
+	TEST(ArcballTest, TestGetArcballPositionForCursorOutsideSphere) {
+		const glm::vec2 cursor_position_ndc{.75f, .85f};
+		const auto arcball_position = GetArcballPosition(cursor_position_ndc);
+		ASSERT_FLOAT_EQ(arcball_position.x, .66162163f);
+		ASSERT_FLOAT_EQ(arcball_position.y, .74983788f);
+		ASSERT_FLOAT_EQ(arcball_position.z, 0.f);
 	}
 }
