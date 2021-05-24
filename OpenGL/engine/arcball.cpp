e5e78f707ce320205515e6b5cb0ee856d903c232
Matthew Rister <matthewrister@outlook.com>
2021-05-23 22:21:47 -0700

    style: rename variables

--- OpenGL/engine/arcball.cpp
+++ OpenGL/engine/arcball.cpp
@@ -11,15 +11,15 @@ namespace {
 	constexpr glm::vec2 GetNormalizedCursorPosition(
 		const glm::dvec2& cursor_position, const std::int32_t window_width, const std::int32_t window_height) {
 
-		const auto x_norm = static_cast<GLfloat>(cursor_position.x * 2.0 / window_width - 1.0);
-		const auto y_norm = static_cast<GLfloat>(cursor_position.y * 2.0 / window_height - 1.0);
+		const auto x_ndc = static_cast<GLfloat>(cursor_position.x * 2.0 / window_width - 1.0);
+		const auto y_ndc = static_cast<GLfloat>(cursor_position.y * 2.0 / window_height - 1.0);
 
-		return {std::clamp(x_norm, -1.f, 1.f), std::clamp(-y_norm, -1.f, 1.f)};
+		return {std::clamp(x_ndc, -1.f, 1.f), std::clamp(-y_ndc, -1.f, 1.f)};
 	}
 
-	glm::vec3 GetArcballPosition(const glm::vec2& cursor_position_norm) {
-		const auto x = cursor_position_norm.x;
-		const auto y = cursor_position_norm.y;
+	glm::vec3 GetArcballPosition(const glm::vec2& cursor_position_ndc) {
+		const auto x = cursor_position_ndc.x;
+		const auto y = cursor_position_ndc.y;
 
 		if (const auto c = x * x + y * y; c <= 1.f) {
 			return glm::vec3{x, y, std::sqrt(1.f - c)};
@@ -33,11 +33,11 @@ std::optional<const std::pair<const glm::vec3, const GLfloat>> arcball::GetRotat
 	const Window& window, const glm::dvec2& cursor_position, const glm::dvec2& prev_cursor_position) {
 
 	const auto [width, height] = window.Size();
-	const auto cursor_position_norm = GetNormalizedCursorPosition(cursor_position, width, height);
-	const auto prev_cursor_position_norm = GetNormalizedCursorPosition(prev_cursor_position, width, height);
+	const auto cursor_position_ndc = GetNormalizedCursorPosition(cursor_position, width, height);
+	const auto prev_cursor_position_ndc = GetNormalizedCursorPosition(prev_cursor_position, width, height);
 
-	const auto arcball_position = GetArcballPosition(cursor_position_norm);
-	const auto prev_arcball_position = GetArcballPosition(prev_cursor_position_norm);
+	const auto arcball_position = GetArcballPosition(cursor_position_ndc);
+	const auto prev_arcball_position = GetArcballPosition(prev_cursor_position_ndc);
 	const auto angle = std::acos(std::min<>(1.f, glm::dot(prev_arcball_position, arcball_position)));
 
 	if (static constexpr GLfloat epsilon = 1e-3f; angle > epsilon) {
