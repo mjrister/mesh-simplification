b727e8992878bafacade86dc3c3c7238f11d11a0
Matthew Rister <matthewrister@outlook.com>
2021-05-24 21:47:10 -0700

    fix: build break due to refactor

--- OpenGLTest/engine/arcball_test.cpp
+++ OpenGLTest/engine/arcball_test.cpp
@@ -1,37 +1,41 @@
 #include <gtest/gtest.h>
 
+#include <utility>
+
 #include "engine/arcball.cpp"
 
 namespace {
 
 	TEST(ArcballTest, TestGetNormalizedDeviceCoordinates) {
-		constexpr auto width = 300, height = 200;
+		constexpr auto width = 300;
+		constexpr auto height = 200;
+		constexpr auto window_dimensions = std::make_pair(width, height);
 
-		constexpr auto cursor_position_ndc0 = GetNormalizedDeviceCoordinates(glm::dvec2{0.f, 0.f}, width, height);
+		constexpr auto cursor_position_ndc0 = GetNormalizedDeviceCoordinates(glm::dvec2{0.f, 0.f}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc0.x, -1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc0.y, 1.f);
 
-		constexpr auto cursor_position_ndc1 = GetNormalizedDeviceCoordinates(glm::dvec2{0.f, height}, width, height);
+		constexpr auto cursor_position_ndc1 = GetNormalizedDeviceCoordinates(glm::dvec2{0.f, height}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc1.x, -1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc1.y, -1.f);
 
-		constexpr auto cursor_position_ndc2 = GetNormalizedDeviceCoordinates(glm::dvec2{width, height}, width, height);
+		constexpr auto cursor_position_ndc2 = GetNormalizedDeviceCoordinates(glm::dvec2{width, height}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc2.x, 1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc2.y, -1.f);
 
-		constexpr auto cursor_position_ndc3 = GetNormalizedDeviceCoordinates(glm::dvec2{width, 0.f}, width, height);
+		constexpr auto cursor_position_ndc3 = GetNormalizedDeviceCoordinates(glm::dvec2{width, 0.f}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc3.x, 1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc3.y, 1.f);
 
-		constexpr auto cursor_position_ndc4 = GetNormalizedDeviceCoordinates(glm::dvec2{width/2.f, height/2.f}, width, height);
+		constexpr auto cursor_position_ndc4 = GetNormalizedDeviceCoordinates(glm::dvec2{width/2.f, height/2.f}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc4.x, 0.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc4.y, 0.f);
 
-		constexpr auto cursor_position_ndc5 = GetNormalizedDeviceCoordinates(glm::dvec2{-1.f, -1.f}, width, height);
+		constexpr auto cursor_position_ndc5 = GetNormalizedDeviceCoordinates(glm::dvec2{-1.f, -1.f}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc5.x, -1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc5.y, 1.f);
 
-		constexpr auto cursor_position_ndc6 = GetNormalizedDeviceCoordinates(glm::dvec2{width + 1.f, height + 1.f}, width, height);
+		constexpr auto cursor_position_ndc6 = GetNormalizedDeviceCoordinates(glm::dvec2{width + 1.f, height + 1.f}, window_dimensions);
 		ASSERT_FLOAT_EQ(cursor_position_ndc6.x, 1.f);
 		ASSERT_FLOAT_EQ(cursor_position_ndc6.y, -1.f);
 	}
