d97b35ae99041128b5049e2fb57635692aee6ce7
Matthew Rister <matthewrister@outlook.com>
2021-05-26 20:27:28 -0700

    fix: use auto and decltype to reduce type conversion

--- OpenGL/geometry/vertex.h
+++ OpenGL/geometry/vertex.h
@@ -14,9 +14,9 @@ namespace geometry {
 		Vertex(const std::uint32_t id, const glm::vec3& position, const glm::vec3& normal)
 			: id_{id}, position_{position}, normal_{normal} {}
 
-		[[nodiscard]] std::int32_t Id() const { return id_; }
-		[[nodiscard]] const glm::vec3& Position() const { return position_; }
-		[[nodiscard]] const glm::vec3& Normal() const { return normal_; }
+		[[nodiscard]] auto Id() const { return id_; }
+		[[nodiscard]] const auto& Position() const { return position_; }
+		[[nodiscard]] const auto& Normal() const { return normal_; }
 
 		friend bool operator==(const Vertex& lhs, const Vertex& rhs) {
 			return lhs.id_ == rhs.id_ && lhs.position_ == rhs.position_ && lhs.normal_ == rhs.normal_;
@@ -28,9 +28,9 @@ namespace geometry {
 
 		friend std::size_t hash_value(const Vertex& vertex) {
 			std::size_t seed = 0x0EAC3880;
-			seed ^= (seed << 6) + (seed >> 2) + 0x7CB4C352 + std::hash<std::uint32_t>{}(vertex.id_);
-			seed ^= (seed << 6) + (seed >> 2) + 0x272DC32C + std::hash<glm::vec3>{}(vertex.position_);
-			seed ^= (seed << 6) + (seed >> 2) + 0x0DE14DA2 + std::hash<glm::vec3>{}(vertex.normal_);
+			seed ^= (seed << 6) + (seed >> 2) + 0x7CB4C352 + std::hash<decltype(vertex.id_)>{}(vertex.id_);
+			seed ^= (seed << 6) + (seed >> 2) + 0x272DC32C + std::hash<decltype(vertex.position_)>{}(vertex.position_);
+			seed ^= (seed << 6) + (seed >> 2) + 0x0DE14DA2 + std::hash<decltype(vertex.normal_)>{}(vertex.normal_);
 			return seed;
 		}
 
