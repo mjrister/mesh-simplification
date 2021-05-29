f1010b17ca3ecbf0f8e6e09b5b6bf8616d26f2d6
Matthew Rister <matthewrister@outlook.com>
2021-05-28 22:19:10 -0700

    test: add vertex tests

---
+++ OpenGLTest/geometry/vertex_test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include "geometry/half_edge.h"
+#include "geometry/vertex.h"
+
+namespace {
+	using namespace geometry;
+
+	TEST(VertexTest, TestGetId) {
+		const Vertex vertex{42, glm::vec4{}, glm::vec3{}};
+		ASSERT_EQ(42, vertex.Id());
+	}
+
+	TEST(VertexTest, TestGetPosition) {
+		const Vertex vertex{0, glm::vec4{42.f}, glm::vec3{}};
+		ASSERT_EQ((glm::vec4{42.f}), vertex.Position());
+	}
+
+	TEST(VertexTest, TestGetNormal) {
+		const Vertex vertex{0, glm::vec4{}, glm::vec3{42.f}};
+		ASSERT_EQ(glm::vec3{42.f}, vertex.Normal());
+	}
+
+	TEST(VertexTest, TestGetEdge) {
+		const auto vertex = std::make_shared<Vertex>(42, glm::vec4{}, glm::vec3{});
+		const auto edge = std::make_shared<HalfEdge>(vertex);
+		vertex->SetEdge(edge);
+		ASSERT_EQ(edge, vertex->Edge());
+	}
+}
