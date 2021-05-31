3f4dd9763c35dfccdb96b6cdd824c500cf34626d
Matthew Rister <matthewrister@outlook.com>
2021-05-30 21:44:23 -0700

    refactor: avoid testing anonymous namespace methods

--- OpenGLTest/graphics/mesh_test.cpp
+++ OpenGLTest/graphics/mesh_test.cpp
@@ -3,13 +3,15 @@
 #include <glm/glm.hpp>
 #include <gtest/gtest.h>
 
-#include "graphics/mesh.cpp"
+#include "graphics/mesh.h"
+
+using namespace gfx;
 
 namespace {
 	TEST(MeshTest, TestValidateMeshWithInvalidNumberOfPositions) {
 		for (std::uint8_t i = 0; i <= static_cast<std::uint8_t>(4); ++i) {
 			if (i != 3) {
-				ASSERT_THROW(Validate(std::vector<glm::vec4>(i), {}, {}, {}), std::invalid_argument);
+				ASSERT_THROW((Mesh{std::vector<glm::vec4>(i), {}, {}, {}}), std::invalid_argument);
 			}
 		}
 	}
@@ -18,7 +20,7 @@ namespace {
 		const std::vector<glm::vec4> positions(3);
 		for (std::uint8_t i = 1; i <= static_cast<std::uint8_t>(4); ++i) {
 			if (i != 3) {
-				ASSERT_THROW(Validate(positions, std::vector<glm::vec2>(i), {}, {}), std::invalid_argument);
+				ASSERT_THROW((Mesh{positions, std::vector<glm::vec2>(i), {}, {}}), std::invalid_argument);
 			}
 		}
 	}
@@ -27,7 +29,7 @@ namespace {
 		const std::vector<glm::vec4> positions(3);
 		for (std::uint8_t i = 1; i <= static_cast<std::uint8_t>(4); ++i) {
 			if (i != 3) {
-				ASSERT_THROW(Validate(positions, {}, std::vector<glm::vec3>(i), {}), std::invalid_argument);
+				ASSERT_THROW((Mesh{positions, {}, std::vector<glm::vec3>(i), {}}), std::invalid_argument);
 			}
 		}
 	}
@@ -36,7 +38,7 @@ namespace {
 		const std::vector<glm::vec4> positions(3);
 		for (std::uint8_t i = 1; i <= static_cast<std::uint8_t>(4); ++i) {
 			if (i != 3) {
-				ASSERT_THROW(Validate(positions, {}, {}, std::vector<GLuint>(i)), std::invalid_argument);
+				ASSERT_THROW((Mesh{positions, {}, {}, std::vector<GLuint>(i)}), std::invalid_argument);
 			}
 		}
 	}
@@ -45,7 +47,7 @@ namespace {
 		const std::vector<glm::vec4> positions(3);
 		const std::vector<glm::vec2> texture_coordinates(3);
 		const std::vector<glm::vec3> normals(3);
-		ASSERT_NO_THROW(Validate(positions, texture_coordinates, normals, {}));
+		ASSERT_NO_THROW((Mesh{positions, texture_coordinates, normals, {}}));
 	}
 
 	TEST(MeshTest, TestValidateMeshWithCorrectNumberOfPositionsTextureCoordinatesNormalsAndIndices) {
@@ -53,6 +55,6 @@ namespace {
 		const std::vector<glm::vec2> texture_coordinates(2);
 		const std::vector<glm::vec3> normals(5);
 		const std::vector<GLuint> indices(3);
-		ASSERT_NO_THROW(Validate(positions, texture_coordinates, normals, indices));
+		ASSERT_NO_THROW((Mesh{positions, texture_coordinates, normals, indices}));
 	}
 }
