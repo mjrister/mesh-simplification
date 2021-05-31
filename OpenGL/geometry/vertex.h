0fdb5cd9372fe6197fd0c6dc7da0c5a42b48a485
Matthew Rister <matthewrister@outlook.com>
2021-05-31 12:05:36 -0700

    fix: remove bug-prone equality operator

--- OpenGL/geometry/vertex.h
+++ OpenGL/geometry/vertex.h
@@ -22,9 +22,6 @@ namespace geometry {
 		[[nodiscard]] std::shared_ptr<HalfEdge> Edge() const { return edge_; }
 		void SetEdge(const std::shared_ptr<HalfEdge>& edge) { edge_ = edge; }
 
-		friend bool operator==(const Vertex& lhs, const Vertex& rhs) { return lhs.id_ == rhs.id_; }
-		friend bool operator!=(const Vertex& lhs, const Vertex& rhs) { return !(lhs == rhs); }
-
 		friend std::ostream& operator<<(std::ostream& os, const Vertex& vertex) { return os << vertex.id_; }
 
 	private:
