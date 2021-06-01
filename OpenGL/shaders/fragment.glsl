c26ccbbcb45c4d3df346fb838a0e9176f198cb82
Matthew Rister <matthewrister@outlook.com>
2021-06-01 14:29:49 -0700

    style: rename variable

--- OpenGL/shaders/fragment.glsl
+++ OpenGL/shaders/fragment.glsl
@@ -36,8 +36,8 @@ vec4 GetFragmentColor() {
 	light_direction = normalize(light_direction);
 
 	// calculate angle between light source and vertex normal
-	vec3 normal = normalize(vertex.normal);
-	float diffuse_intensity = max(dot(light_direction, normal), 0.f);
+	vec3 vertex_normal = normalize(vertex.normal);
+	float diffuse_intensity = max(dot(light_direction, vertex_normal), 0.f);
 
 	// direct light contribution should not be considered if the angle between
 	// the light source and vertex normal is greater than or equal to 90 degrees
@@ -45,7 +45,7 @@ vec4 GetFragmentColor() {
 		vec3 diffuse_color = diffuse_intensity * material.diffuse;
 
 		// calculate the angle between the reflected light and view direction
-		vec3 reflect_direction = normalize(reflect(-light_direction, normal));
+		vec3 reflect_direction = normalize(reflect(-light_direction, vertex_normal));
 		vec3 view_direction = normalize(-vertex.position.xyz);
 		float specular_intensity = pow(max(dot(reflect_direction, view_direction), 0.f), material.shininess);
 		vec3 specular_color = specular_intensity * material.specular;
