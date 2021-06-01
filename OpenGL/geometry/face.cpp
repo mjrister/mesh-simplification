0277c1cd83fe0c5a3a60eaaeeb186e6639652c7a
Matthew Rister <matthewrister@outlook.com>
2021-05-31 20:13:00 -0700

    refactor: use std::array instead of tuple

--- OpenGL/geometry/face.cpp
+++ OpenGL/geometry/face.cpp
@@ -1,8 +1,8 @@
 #include "face.h"
 
 #include <algorithm>
+#include <array>
 #include <sstream>
-#include <tuple>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/epsilon.hpp>
@@ -18,34 +18,37 @@ namespace {
 		return seed;
 	}
 
-	auto GetMinVertexOrder(
+	std::array<std::shared_ptr<Vertex>, 3> GetMinVertexOrder(
 		const std::shared_ptr<Vertex>& v0,
 		const std::shared_ptr<Vertex>& v1,
 		const std::shared_ptr<Vertex>& v2) {
 
 		if (const auto min_id = std::min<>({v0->Id(), v1->Id(), v2->Id()}); min_id == v0->Id()) {
-			return make_tuple(v0, v1, v2);
+			return {v0, v1, v2};
 		} else if (min_id == v1->Id()) {
-			return make_tuple(v1, v2, v0);
+			return {v1, v2, v0};
 		} else {
-			return make_tuple(v2, v0, v1);
+			return {v2, v0, v1};
 		}
 	}
 
-	bool IsTriangle(const Vertex& v0, const Vertex& v1, const Vertex& v2) {
+	bool IsCollinear(const Vertex& v0, const Vertex& v1, const Vertex& v2) {
 		const glm::vec3 edge01 = v1.Position() - v0.Position();
 		const glm::vec3 edge02 = v2.Position() - v0.Position();
-		return cross(edge01, edge02) != glm::vec3{0.f};
+		return glm::cross(edge01, edge02) == glm::vec3{0.f};
 	}
 }
 
 Face::Face(
 	const std::shared_ptr<Vertex>& v0, const std::shared_ptr<Vertex>& v1, const std::shared_ptr<Vertex>& v2) {
 
-	std::tie(v0_, v1_, v2_) = GetMinVertexOrder(v0, v1, v2);
+	const auto vertex_order = GetMinVertexOrder(v0, v1, v2);
+	v0_ = vertex_order[0];
+	v1_ = vertex_order[1];
+	v2_ = vertex_order[2];
 	id_ = GetFaceId(*v0_, *v1_, *v2_);
 
-	if (!IsTriangle(*v0_, *v1_, *v2_)) {
+	if (IsCollinear(*v0_, *v1_, *v2_)) {
 		std::ostringstream oss;
 		oss << *this << " is not a triangle";
 		throw std::invalid_argument{oss.str()};
