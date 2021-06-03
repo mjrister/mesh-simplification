6a35801f6fa7d3d4f7a9f5384a5ab444269c26e7
Matthew Rister <matthewrister@outlook.com>
2021-06-03 10:54:28 -0700

    feat: create half edge entry struct

--- OpenGL/geometry/mesh_simplifier.h
+++ OpenGL/geometry/mesh_simplifier.h
@@ -18,12 +18,12 @@ namespace geometry {
 
 	class MeshSimplifier {
 
-		static glm::mat4 ComputeQuadric(const Vertex& v0) {
+		static glm::mat4 ComputeQuadric(const Vertex& vertex) {
 			glm::mat4 quadric{0.f};
 
-			for (auto edgeij = v0.Edge()->Next()->Flip(); edgeij != v0.Edge(); edgeij = edgeij->Next()->Flip()) {
-				const auto& position = v0.Position();
-				const auto& normal = edgeij->Face()->Normal();
+			for (auto edge = vertex.Edge()->Next()->Flip(); edge != vertex.Edge(); edge = edge->Next()->Flip()) {
+				const auto& position = vertex.Position();
+				const auto& normal = edge->Face()->Normal();
 				const auto a = normal.x;
 				const auto b = normal.y;
 				const auto c = normal.z;
@@ -38,13 +38,13 @@ namespace geometry {
 			return quadric;
 		}
 
-		static std::pair<std::shared_ptr<Vertex>, GLfloat> GetEdgeContractionVertex(
-			HalfEdgeMesh& mesh,
-			const std::shared_ptr<const HalfEdge>& edge01,
-			const std::unordered_map<std::size_t, const glm::mat4>& quadrics) {
+		static std::pair<std::shared_ptr<Vertex>, GLfloat> GetOptimalEdgeContractionVertex(
+			const HalfEdge& edge01,
+			const std::unordered_map<std::size_t, const glm::mat4>& quadrics,
+			const std::size_t next_vertex_id) {
 
-			const auto v0 = edge01->Flip()->Vertex();
-			const auto v1 = edge01->Vertex();
+			const auto v0 = edge01.Flip()->Vertex();
+			const auto v1 = edge01.Vertex();
 
 			const auto& K0 = quadrics.at(v0->Id());
 			const auto& K1 = quadrics.at(v1->Id());
@@ -56,22 +56,43 @@ namespace geometry {
 
 			constexpr auto epsilon = std::numeric_limits<GLfloat>::epsilon();
 			if (std::abs(glm::determinant(Q)) < epsilon || std::abs(d) < epsilon) {
-				const auto v_new = Vertex::Average(mesh.NextVertexId(), *v0, *v1);
-				return { std::make_shared<Vertex>(v_new), 0.f };
+				const auto v_new = Vertex::Average(next_vertex_id, *v0, *v1);
+				return {std::make_shared<Vertex>(v_new), 0.f};
 			}
 
-			const auto Q_inv = glm::inverse(Q);
-			const auto D_inv = glm::column(glm::mat4{Q_inv}, 3, glm::vec4{1.f / d * Q_inv * b, 1.f / d});
+			const auto Q_inv = inverse(Q);
+			const auto D_inv = column(glm::mat4{Q_inv}, 3, glm::vec4{1.f / d * Q_inv * b, 1.f / d});
 
 			auto position = D_inv * glm::vec4{0.f, 0.f, 0.f, 1.f};
 			position /= position.w;
 			const auto normal = (v0->Normal() + v1->Normal()) / 2.f;
 			const auto cost = glm::dot(position, K01 * position);
 
-			return { std::make_shared<Vertex>(mesh.NextVertexId(), position, normal), cost };
+			return {std::make_shared<Vertex>(next_vertex_id, position, normal), cost};
 		}
 
-		void Simplify(const HalfEdgeMesh& mesh, const GLfloat stop_ratio) {
+		struct HalfEdgeEntry {
+
+			HalfEdgeEntry(
+				const std::shared_ptr<HalfEdge>& edge,
+				const std::unordered_map<std::size_t, const glm::mat4>& quadrics,
+				const std::size_t next_vertex_id)
+				: edge{edge} {
+
+				const auto vertex_and_cost = GetOptimalEdgeContractionVertex(*edge, quadrics, next_vertex_id);
+				vertex = vertex_and_cost.first;
+				cost = vertex_and_cost.second;
+			}
+
+			friend bool operator<(const HalfEdgeEntry& a, const HalfEdgeEntry& b) { return a.cost < b.cost; }
+
+			std::shared_ptr<HalfEdge> edge;
+			std::shared_ptr<Vertex> vertex;
+			GLfloat cost;
+			bool most_recent = true;
+		};
+
+		void Simplify(HalfEdgeMesh& mesh, const GLfloat stop_ratio) {
 
 			std::unordered_map<std::size_t, glm::mat4> quadrics;
 			for (const auto& [vertex_id, vertex] : mesh.Vertices()) {
@@ -79,27 +100,20 @@ namespace geometry {
 			}
 
 			std::unordered_map<std::size_t, std::shared_ptr<HalfEdge>> valid_edges;
-			std::priority_queue<std::shared_ptr<HalfEdge>> edge_queue;
+			std::priority_queue<HalfEdgeEntry> edge_queue;
 
 			for (const auto& [_, edge] : mesh.Edges()) {
-				const auto min_edge = std::min<>(edge, edge->Flip(), [](const auto a, const auto b) {
+				const auto min_edge = std::min<>(edge, edge->Flip(), [](const auto& a, const auto& b) {
 					return a->Vertex()->Id() < b->Vertex()->Id();
 				});
 				if (const auto min_edge_key = hash_value(*min_edge); !valid_edges.count(min_edge_key)) {
 					valid_edges.emplace(min_edge_key, min_edge);
-					edge_queue.emplace(min_edge);
+					edge_queue.emplace(min_edge, quadrics, mesh.NextVertexId());
 				}
 			}
 
 			const auto face_count = mesh.Faces().size();
 			while (!edge_queue.empty() && face_count * stop_ratio > mesh.Faces().size()) {
-
-				const auto entry = edge_queue.top();
-				edge_queue.pop();
-
-				if (valid_edges.count(hash_value(*entry))) {
-
-				}
 			}
 		}
 	};
