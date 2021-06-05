05d91e6bebd1b3bea4650a2cc6d6329fa4852fab
Matthew Rister <matthewrister@outlook.com>
2021-06-04 21:36:02 -0700

    refactor: use std::tie/tuple to assign face vertices

--- OpenGL/geometry/face.cpp
+++ OpenGL/geometry/face.cpp
@@ -1,48 +1,43 @@
 #include "geometry/face.h"
 
 #include <algorithm>
-#include <array>
 #include <sstream>
+#include <tuple>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/epsilon.hpp>
 
 namespace {
-	std::array<std::shared_ptr<const geometry::Vertex>, 3> GetMinVertexOrder(
+	auto GetMinVertexOrder(
 		const std::shared_ptr<const geometry::Vertex>& v0,
 		const std::shared_ptr<const geometry::Vertex>& v1,
 		const std::shared_ptr<const geometry::Vertex>& v2) {
 
 		if (const auto min_id = std::min<>({v0->Id(), v1->Id(), v2->Id()}); min_id == v0->Id()) {
-			return {v0, v1, v2};
+			return std::make_tuple(v0, v1, v2);
 		} else if (min_id == v1->Id()) {
-			return {v1, v2, v0};
+			return std::make_tuple(v1, v2, v0);
 		} else {
-			return {v2, v0, v1};
+			return std::make_tuple(v2, v0, v1);
 		}
 	}
 
 	glm::vec3 GetFaceNormal(const geometry::Vertex& v0, const geometry::Vertex& v1, const geometry::Vertex& v2) {
 		const glm::vec3 edge01 = v1.Position() - v0.Position();
 		const glm::vec3 edge02 = v2.Position() - v0.Position();
-		return glm::normalize(glm::cross(edge01, edge02));
+		const auto normal = glm::normalize(glm::cross(edge01, edge02));
+		if (glm::any(glm::isnan(normal))) {
+			std::ostringstream oss;
+			oss << '(' << v0 << ',' << v1 << ',' << v2 << ") is not a triangle";
+			throw std::invalid_argument{ oss.str() };
+		}
+		return normal;
 	}
 }
 
 geometry::Face::Face(
 	const std::shared_ptr<const Vertex>& v0,
 	const std::shared_ptr<const Vertex>& v1,
-	const std::shared_ptr<const Vertex>& v2) {
-
-	const auto vertex_order = GetMinVertexOrder(v0, v1, v2);
-	v0_ = vertex_order[0];
-	v1_ = vertex_order[1];
-	v2_ = vertex_order[2];
-	normal_ = GetFaceNormal(*v0_, *v1_, *v2_);
-
-	if (glm::any(glm::isnan(normal_))) {
-		std::ostringstream oss;
-		oss << *this << " is not a triangle";
-		throw std::invalid_argument{oss.str()};
-	}
+	const std::shared_ptr<const Vertex>& v2)
+	: normal_{GetFaceNormal(*v0, *v1, *v2)} {
+	std::tie(v0_, v1_, v2_) = GetMinVertexOrder(v0, v1, v2);
 }
