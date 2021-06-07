24ea84d556c2af3ae76907d7e687dad22e11c477
Matthew Rister <matthewrister@outlook.com>
2021-06-06 19:12:29 -0700

    refactor: remove obvious comments that do not add any value

--- OpenGL/geometry/face.h
+++ OpenGL/geometry/face.h
@@ -8,36 +8,22 @@
 namespace geometry {
 	class HalfEdge;
 
-	/** \brief A triangle face defined by three vertices in counter-clockwise winding order. */
 	class Face {
 
 	public:
-		/**
-		 * \brief Initializes a triangle face.
-		 * \param v0,v1,v2 The face vertices.
-		 */
 		Face(const std::shared_ptr<const Vertex>& v0,
 		     const std::shared_ptr<const Vertex>& v1,
 		     const std::shared_ptr<const Vertex>& v2);
 
-		/** \brief Gets the first face vertex. */
 		[[nodiscard]] std::shared_ptr<const Vertex> V0() const { return v0_; }
-
-		/** \brief  Gets the second face vertex. */
 		[[nodiscard]] std::shared_ptr<const Vertex> V1() const { return v1_; }
-
-		/** \brief Gets the third face vertex. */
 		[[nodiscard]] std::shared_ptr<const Vertex> V2() const { return v2_; }
-
-		/** \brief  Gets the face normal. */
 		[[nodiscard]] const glm::vec3& Normal() const { return normal_; }
 
-		/** \brief Sends a string representation of a face to an output stream. */
 		friend std::ostream& operator<<(std::ostream& os, const Face& face) {
 			return os << '(' << *face.v0_ << ',' << *face.v1_ << ',' << *face.v2_ << ')';
 		}
 
-		/** \brief Gets the face hash value. */
 		friend std::size_t hash_value(const Face& face) {
 			return hash_value(*face.v0_, *face.v1_, *face.v2_);
 		}
