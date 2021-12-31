561769084617927678d9131afbf019f46bef01f2
Matthew Rister <matthewrister@outlook.com>
2021-12-30 20:05:03 -0800

    test: add get face area and normal tests

--- MeshSimplificationTest/geometry/face_test.cpp
+++ MeshSimplificationTest/geometry/face_test.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 namespace {
 	array<shared_ptr<Vertex>, 3> MakeTriangle() {
-		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f}, vec3{});
-		const auto v1 = make_shared<Vertex>(1, vec3{0.f, .5f, 0.f}, vec3{});
-		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f}, vec3{});
+		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f});
+		const auto v1 = make_shared<Vertex>(1, vec3{1.f, -1.f, 0.f});
+		const auto v2 = make_shared<Vertex>(2, vec3{0.f, .5f, 0.f});
 		return {v0, v1, v2};
 	}
 
@@ -38,10 +38,22 @@ namespace {
 	}
 
 	TEST(FaceTest, TestFaceInitializationWithCollinearVerticesThrowsException) {
-		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f}, vec3{});
-		const auto v1 = make_shared<Vertex>(1, vec3{0.f, -1.f, 0.f}, vec3{});
-		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f}, vec3{});
+		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f});
+		const auto v1 = make_shared<Vertex>(1, vec3{0.f, -1.f, 0.f});
+		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f});
 		ASSERT_THROW((Face{v0, v1, v2}), invalid_argument);
+	}
+
+	TEST(FaceTest, TestGetFaceArea) {
+		const auto [v0, v1, v2] = MakeTriangle();
+		const Face face012{v0, v1, v2};
+		ASSERT_FLOAT_EQ(1.5f, face012.Area());
+	}
+
+	TEST(FaceTEst, TestGetFaceNormal) {
+		const auto [v0, v1, v2] = MakeTriangle();
+		const Face face012{v0, v1, v2};
+		ASSERT_EQ((vec3{0.f, 0.f, 1.f}), face012.Normal());
 	}
 
 	TEST(FaceTest, TestFormatFace) {
@@ -50,17 +62,17 @@ namespace {
 	}
 
 	TEST(FaceTest, TestEqualFacesProduceTheSameHashValue) {
-		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f}, vec3{});
-		const auto v1 = make_shared<Vertex>(1, vec3{0.f, .5f, 0.f}, vec3{});
-		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f}, vec3{});
+		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f});
+		const auto v1 = make_shared<Vertex>(1, vec3{0.f, .5f, 0.f});
+		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f});
 		const Face face012{v0, v1, v2};
 		ASSERT_EQ(hash_value(face012), hash_value(Face{face012}));
 	}
 
 	TEST(FaceTest, TestThreeVerticesProduceSameHashValueAsFace) {
-		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f}, vec3{});
-		const auto v1 = make_shared<Vertex>(1, vec3{0.f, .5f, 0.f}, vec3{});
-		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f}, vec3{});
+		const auto v0 = make_shared<Vertex>(0, vec3{-1.f, -1.f, 0.f});
+		const auto v1 = make_shared<Vertex>(1, vec3{0.f, .5f, 0.f});
+		const auto v2 = make_shared<Vertex>(2, vec3{1.f, -1.f, 0.f});
 		const Face face012{v0, v1, v2};
 		ASSERT_EQ(hash_value(*v0, *v1, *v2), hash_value(face012));
 	}
