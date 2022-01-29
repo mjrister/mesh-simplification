0789db0da89bba0d9d94a746db16e455a4cd7c62
Matthew Rister <matthewrister@outlook.com>
2022-01-29 15:27:40 -0800

    docs: add doxygen documentation

--- MeshSimplification/app/scene.h
+++ MeshSimplification/app/scene.h
@@ -6,10 +6,21 @@
 
 namespace app {
 
+/** \brief Defines all of the necessary objects (e.g., camera, lighting, mesh, etc.) to render the scene. */
 class Scene {
 
 public:
+	/**
+	 * \brief Initializes the scene.
+	 * \param window A reference to the window to render to.
+	 * \param shader_program The shader shader program to use for rendering.
+	 */
 	Scene(Window* window, gfx::ShaderProgram* shader_program);
+
+	/**
+	 * \brief Renders the scene.
+	 * \param delta_time The amount of time elapsed since the last frame was rendered.
+	 */
 	void Render(float delta_time);
 
 private:
