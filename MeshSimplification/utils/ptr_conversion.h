bb8c36e8aee5e8f860a0d3217f8033645170e19b
Matthew Rister <matthewrister@outlook.com>
2022-01-30 14:07:21 -0800

    test:: add weak_ptr conversion tests

--- MeshSimplification/utils/ptr_conversion.h
+++ MeshSimplification/utils/ptr_conversion.h
@@ -6,7 +6,7 @@
 namespace ptr {
 
 template <typename T>
-[[nodiscard]] static std::shared_ptr<T> Get(const std::weak_ptr<T>& weak_t) {
+static std::shared_ptr<T> Get(const std::weak_ptr<T>& weak_t) {
 	if (auto shared_t = weak_t.lock()) return shared_t;
 	throw std::runtime_error{"Attempted to access a dangling pointer"};
 }
---
+++ MeshSimplificationTest/ptr_conversion_test.cpp
@@ -0,0 +1,26 @@
+#include "utils/ptr_conversion.h"
+
+#include <memory>
+#include <stdexcept>
+
+#include <gtest/gtest.h>
+
+namespace {
+using namespace ptr;
+using namespace std;
+
+TEST(PtrConversion, TestValidWeakPointerConversionToSharedPointer) {
+	const auto meaning_of_life = make_shared<const int>(42);
+	const std::weak_ptr weak_ptr = meaning_of_life;
+	ASSERT_EQ(42, *Get(weak_ptr));
+}
+
+TEST(PtrConversion, TestInvalidWeakPointerConversionToSharedPointer) {
+	std::weak_ptr<const int> weak_ptr;
+	{
+		const auto meaning_of_life = make_shared<const int>(42);
+		weak_ptr = meaning_of_life;
+	}
+	ASSERT_THROW(Get(weak_ptr), runtime_error);
+}
+}
