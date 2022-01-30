152e8926865d8dec585e7839c05822ca301a3fd2
Matthew Rister <matthewrister@outlook.com>
2022-01-30 14:41:36 -0800

    docs: add doxygen documentation

--- MeshSimplification/utils/ptr_conversion.h
+++ MeshSimplification/utils/ptr_conversion.h
@@ -5,6 +5,13 @@
 
 namespace ptr {
 
+/**
+ * \brief Converts a weak pointer to a shared pointer.
+ * \tparam T The shared pointer type.
+ * \param weak_t The weak pointer to convert.
+ * \return The weak pointer converted to a shared pointer.
+ * \throw std::runtime_error Indicates the weak pointer is expired.
+ */
 template <typename T>
 static std::shared_ptr<T> Get(const std::weak_ptr<T>& weak_t) {
 	if (auto shared_t = weak_t.lock()) return shared_t;
