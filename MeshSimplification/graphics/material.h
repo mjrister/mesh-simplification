276d58da9cf9183584b3e9a21a4d0e0484234bad
Matthew Rister <matthewrister@outlook.com>
2022-02-09 22:23:15 -0800

    refactor: convert material to a struct

--- MeshSimplification/graphics/material.h
+++ MeshSimplification/graphics/material.h
@@ -26,12 +26,11 @@ enum class MaterialType {
 };
 
 /**
- * \brief A type of material with light reflectance properties.
+ * \brief A type of material with light reflectance properties used in the Phong reflection model.
  * \note Material properties courtesy of http://devernay.free.fr/cours/opengl/materials.html
  */
-class Material {
+struct Material {
 
-public:
 	/**
 	 * \brief A static factory function to construct a material from a dedicated material type.
 	 * \param material_type The material type representing the material to construct.
@@ -40,124 +39,116 @@ public:
 	static constexpr Material FromType(const MaterialType material_type) {
 		switch (material_type) {
 			case MaterialType::kBrass: {
-				constexpr glm::vec3 kAmbient{.329412f, .223529f, .027451f};
-				constexpr glm::vec3 kDiffuse{.780392f, .568627f, .113725f};
-				constexpr glm::vec3 kSpecular{.992157f, .941176f, .807843f};
-				constexpr auto kShininess = .21794872f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.329412f, .223529f, .027451f},
+					.diffuse = glm::vec3{.780392f, .568627f, .113725f},
+					.specular = glm::vec3{.992157f, .941176f, .807843f},
+					.shininess = .21794872f,
+				};
 			}
 			case MaterialType::kBronze: {
-				constexpr glm::vec3 kAmbient{.2125f, .1275f, .054f};
-				constexpr glm::vec3 kDiffuse{.714f, .4284f, .18144f};
-				constexpr glm::vec3 kSpecular{.393548f, .271906f, .166721f};
-				constexpr auto kShininess = .2f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
-			};
+				return Material{
+					.ambient = glm::vec3{.2125f, .1275f, .054f},
+					.diffuse = glm::vec3{.714f, .4284f, .18144f},
+					.specular = glm::vec3{.393548f, .271906f, .166721f},
+					.shininess = .2f
+				};
+			}
 			case MaterialType::kChrome: {
-				constexpr glm::vec3 kAmbient{.25f, .25f, .25f};
-				constexpr glm::vec3 kDiffuse{.4f, .4f, .4f};
-				constexpr glm::vec3 kSpecular{.774597f, .774597f, .774597f};
-				constexpr auto kShininess = .6f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.25f, .25f, .25f},
+					.diffuse = glm::vec3{.4f, .4f, .4f},
+					.specular = glm::vec3{.774597f, .774597f, .774597f},
+					.shininess = .6f
+				};
 			}
 			case MaterialType::kCopper: {
-				constexpr glm::vec3 kAmbient{.19125f, .0735f, .0225f};
-				constexpr glm::vec3 kDiffuse{.7038f, .27048f, .0828f};
-				constexpr glm::vec3 kSpecular{.256777f, .137622f, .086014f};
-				constexpr auto kShininess = .1f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.19125f, .0735f, .0225f},
+					.diffuse = glm::vec3{.7038f, .27048f, .0828f},
+					.specular = glm::vec3{.256777f, .137622f, .086014f},
+					.shininess = .6f
+				};
 			}
 			case MaterialType::kEmerald: {
-				constexpr glm::vec3 kAmbient{.0215f, .1745f, .0215f};
-				constexpr glm::vec3 kDiffuse{.07568f, .61424f, .07568f};
-				constexpr glm::vec3 kSpecular{.633f, .727811f, .633f};
-				constexpr auto kShininess = .6f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.0215f, .1745f, .0215f},
+					.diffuse = glm::vec3{.07568f, .61424f, .07568f},
+					.specular = glm::vec3{.633f, .727811f, .633f},
+					.shininess = .6f
+				};
 			}
 			case MaterialType::kGold: {
-				constexpr glm::vec3 kAmbient{.24725f, .1995f, .0745f};
-				constexpr glm::vec3 kDiffuse{.75164f, .60648f, .22648f};
-				constexpr glm::vec3 kSpecular{.628281f, .555802f, .366065f};
-				constexpr auto kShininess = .4f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.24725f, .1995f, .0745f},
+					.diffuse = glm::vec3{.75164f, .60648f, .22648f},
+					.specular = glm::vec3{.628281f, .555802f, .366065f},
+					.shininess = .4f
+				};
 			}
 			case MaterialType::kJade: {
-				constexpr glm::vec3 kAmbient{.135f, .2225f, .1575f};
-				constexpr glm::vec3 kDiffuse{.54f, .89f, .63f};
-				constexpr glm::vec3 kSpecular{.316228f, .316228f, .316228f};
-				constexpr auto kShininess = .1f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.135f, .2225f, .1575f},
+					.diffuse = glm::vec3{.54f, .89f, .63f},
+					.specular = glm::vec3{.316228f, .316228f, .316228f},
+					.shininess = .1f
+				};
 			}
 			case MaterialType::kObsidian: {
-				constexpr glm::vec3 kAmbient{.05375f, .05f, .06625f};
-				constexpr glm::vec3 kDiffuse{.18275f, .17f, .22525f};
-				constexpr glm::vec3 kSpecular{.332741f, .328634f, .346435f};
-				constexpr auto kShininess = .3f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.05375f, .05f, .06625f},
+					.diffuse = glm::vec3{.18275f, .17f, .22525f},
+					.specular = glm::vec3{.332741f, .328634f, .346435f},
+					.shininess = .3f
+				};
 			}
 			case MaterialType::kPearl: {
-				constexpr glm::vec3 kAmbient{.25f, .20725f, .20725f};
-				constexpr glm::vec3 kDiffuse{1.f, .829f, .829f};
-				constexpr glm::vec3 kSpecular{.296648f, .296648f, .296648f};
-				constexpr auto kShininess = .088f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.25f, .20725f, .20725f},
+					.diffuse = glm::vec3{1.f, .829f, .829f},
+					.specular = glm::vec3{.296648f, .296648f, .296648f},
+					.shininess = .088f
+				};
 			}
 			case MaterialType::kRuby: {
-				constexpr glm::vec3 kAmbient{.1745f, .01175f, .01175f};
-				constexpr glm::vec3 kDiffuse{.61424f, .04136f, .04136f};
-				constexpr glm::vec3 kSpecular{.727811f, .626959f, .626959f};
-				constexpr auto kShininess = .6f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.1745f, .01175f, .01175f},
+					.diffuse = glm::vec3{.61424f, .04136f, .04136f},
+					.specular = glm::vec3{.727811f, .626959f, .626959f},
+					.shininess = .6f
+				};
 			}
 			case MaterialType::kSilver: {
-				constexpr glm::vec3 kAmbient{.19225f, .19225f, .19225f};
-				constexpr glm::vec3 kDiffuse{.50754f, .50754f, .50754f};
-				constexpr glm::vec3 kSpecular{.508273f, .508273f, .508273f};
-				constexpr auto kShininess = .4f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.19225f, .19225f, .19225f},
+					.diffuse = glm::vec3{.50754f, .50754f, .50754f},
+					.specular = glm::vec3{.508273f, .508273f, .508273f},
+					.shininess = .4f
+				};
 			}
 			case MaterialType::kTurquoise: {
-				constexpr glm::vec3 kAmbient{.1f, .18725f, .1745f};
-				constexpr glm::vec3 kDiffuse{.396f, .74151f, .69102f};
-				constexpr glm::vec3 kSpecular{.297254f, .30829f, .306678f};
-				constexpr auto kShininess = .1f;
-				return Material{kAmbient, kDiffuse, kSpecular, kShininess};
+				return Material{
+					.ambient = glm::vec3{.1f, .18725f, .1745f},
+					.diffuse = glm::vec3{.396f, .74151f, .69102f},
+					.specular = glm::vec3{.297254f, .30829f, .306678f},
+					.shininess = .1f
+				};
 			}
 			default:
 				throw std::invalid_argument{"Invalid material type"};
 		}
 	}
 
-	/**
-	 * \brief Initializes a material.
-	 * \param ambient The ambient light reflected.
-	 * \param diffuse The diffuse light reflected.
-	 * \param specular The specular light reflected.
-	 * \param shininess The degree of shininess reflected from specular highlights.
-	 */
-	constexpr Material(
-		const glm::vec3& ambient, const glm::vec3& diffuse, const glm::vec3& specular, const float shininess)
-		: ambient_{ambient},
-		  diffuse_{diffuse},
-		  specular_{specular},
-		  shininess_{shininess} {}
+	/** The ambient light reflected. */
+	glm::vec3 ambient;
 
-	/** \brief Gets the ambient light reflected. */
-	[[nodiscard]] constexpr glm::vec3 ambient() const { return ambient_; }
+	/** The diffuse light reflected. */
+	glm::vec3 diffuse;
 
-	/** \brief Gets the diffuse light reflected. */
-	[[nodiscard]] constexpr glm::vec3 diffuse() const { return diffuse_; }
+	/** The specular light reflected. */
+	glm::vec3 specular;
 
-	/** \brief Gets the specular light reflected. */
-	[[nodiscard]] constexpr glm::vec3 specular() const { return specular_; }
-
-	/** \brief Gets the  degree of shininess reflected from specular highlights. */
-	[[nodiscard]] constexpr float shininess() const { return shininess_; }
-
-private:
-	glm::vec3 ambient_;
-	glm::vec3 diffuse_;
-	glm::vec3 specular_;
-	float shininess_;
+	/** The degree of shininess reflected from specular highlights. */
+	float shininess;
 };
 }
--- MeshSimplification/graphics/scene.cpp
+++ MeshSimplification/graphics/scene.cpp
@@ -60,11 +60,11 @@ void InitializeMesh(ShaderProgram& shader_program, Mesh& mesh) {
 	mesh.Scale(vec3{.3f});
 	mesh.Translate(vec3{.5f, -1.f, 0.f});
 
-	constexpr auto kMeshMaterial = Material::FromType(MaterialType::kJade);
-	shader_program.SetUniform("material.ambient", kMeshMaterial.ambient());
-	shader_program.SetUniform("material.diffuse", kMeshMaterial.diffuse());
-	shader_program.SetUniform("material.specular", kMeshMaterial.specular());
-	shader_program.SetUniform("material.shininess", kMeshMaterial.shininess() * 128.f);
+	constexpr auto kMaterial = Material::FromType(MaterialType::kJade);
+	shader_program.SetUniform("material.ambient", kMaterial.ambient);
+	shader_program.SetUniform("material.diffuse", kMaterial.diffuse);
+	shader_program.SetUniform("material.specular", kMaterial.specular);
+	shader_program.SetUniform("material.shininess", kMaterial.shininess * 128.f);
 }
 
 void InitializePointLights(ShaderProgram& shader_program) {
