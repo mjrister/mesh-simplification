5b5bc56af18a3c0849b8be0c8eb1ab1d171c1356
Matthew Rister <matthewrister@outlook.com>
2022-04-09 09:57:38 -0700

    docs: update comment

--- MeshSimplification/shaders/mesh_vertex.glsl
+++ MeshSimplification/shaders/mesh_vertex.glsl
@@ -13,7 +13,7 @@ out Vertex {
 } vertex;
 
 void main() {
-	// generally, normals should be transformed by the upper 3x3 inverse transpose of the view model matrix. In this context,
+	// generally, normals should be transformed by the upper 3x3 inverse transpose of the model-view matrix. In this context,
 	// it is sufficient to use the model-view matrix to transform normals because the mesh is only transformed by rotations
 	// and translations (which are orthogonal matrices with the property that their inverse is equal to their transpose) in
 	// addition to uniform unscaling which is undone when the transformed normal is renomalized.
