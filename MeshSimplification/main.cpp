d78285d7df4630f412bffdacd1d3ea23da003870
Matthew Rister <matthewrister@outlook.com>
2023-10-25 00:00:59 -0700

    refactor: remove frame_timer

--- MeshSimplification/main.cpp
+++ MeshSimplification/main.cpp
@@ -1,38 +1,37 @@
+#include <chrono>
 #include <cstdlib>
 #include <exception>
 #include <format>
 #include <iostream>
 #include <utility>
 
-#include "graphics/frame_timer.h"
 #include "graphics/scene.h"
 #include "graphics/window.h"
 
 int main() {
   try {
+    constexpr auto* kProjectTitle = "Mesh Simplification";
     constexpr auto kWindowDimensions = std::make_pair(1280, 960);
     constexpr auto kOpenGlVersion = std::make_pair(4, 6);
-    constexpr auto* kProjectTitle = "Mesh Simplification";
     qem::Window window{kProjectTitle, kWindowDimensions, kOpenGlVersion};
     qem::Scene scene{&window};
-    qem::FrameTimer frame_timer;
 
-    for (auto elapsed_time = 0.0f; !window.IsClosed();) {
-      frame_timer.Update();
-      const auto delta_time = frame_timer.delta_time();
+    auto current_time = std::chrono::steady_clock::now();
+    auto previous_time = current_time;
 
-      if (elapsed_time += delta_time; elapsed_time > frame_timer.sample_time()) {
-        const auto frames_per_second = static_cast<std::uint32_t>(frame_timer.frames_per_second());
-        const auto window_title = std::format("{} - Frames per second: {}", kProjectTitle, frames_per_second);
-        window.SetTitle(window_title.c_str());
-        elapsed_time = 0.0f;
-      }
+    while (!window.IsClosed()) {
+      const auto delta_time = std::chrono::duration<float>(current_time - previous_time).count();
+      previous_time = current_time;
+      current_time = std::chrono::steady_clock::now();
 
       window.Update();
       scene.Render(delta_time);
     }
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  } catch (...) {
+    std::cerr << "An unknown error occurred" << std::endl;
     return EXIT_FAILURE;
   }
 
