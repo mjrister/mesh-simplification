357da09d262e893540e00007922510b952c478a3
Matthew Rister <matthewrister@outlook.com>
2023-10-31 23:32:05 -0700

    test: rename tests

--- tests/geometry/half_edge_test.cpp
+++ tests/geometry/half_edge_test.cpp
@@ -7,8 +7,8 @@ namespace {
 class HalfEdgeTest : public testing::Test {
 protected:
   HalfEdgeTest()
-      : v0_{std::make_shared<qem::Vertex>(0, glm::vec3{1.0f})},
-        v1_{std::make_shared<qem::Vertex>(1, glm::vec3{2.0f, 0.0f, 0.0f})},
+      : v0_{std::make_shared<qem::Vertex>(0, glm::vec3{0.0f})},
+        v1_{std::make_shared<qem::Vertex>(1, glm::vec3{0.0f})},
         edge01_{std::make_shared<qem::HalfEdge>(v1_)},
         edge10_{std::make_shared<qem::HalfEdge>(v0_)} {
     edge01_->set_flip(edge10_);
@@ -19,12 +19,15 @@ protected:
   std::shared_ptr<qem::HalfEdge> edge01_, edge10_;
 };
 
-TEST_F(HalfEdgeTest, TestEqualHalfEdgesProduceTheSameHashValue) {
+TEST_F(HalfEdgeTest, TestEqualHalfEdgesHaveTheSameHashValue) {
   EXPECT_EQ(hash_value(*edge01_), hash_value(qem::HalfEdge{*edge01_}));
+}
+
+TEST_F(HalfEdgeTest, TestOppositeHalfEdgesDoNotHaveTheSameHashValue) {
   EXPECT_NE(hash_value(*edge01_), hash_value(*edge01_->flip()));
 }
 
-TEST_F(HalfEdgeTest, TestTwoVerticesProduceSameHashValueAsHalfEdge) {
+TEST_F(HalfEdgeTest, TestEqualHalfEdgeVerticesHaveTheSameHashValue) {
   const auto v0 = edge01_->flip()->vertex();
   const auto v1 = edge01_->vertex();
   EXPECT_EQ(hash_value(*v0, *v1), hash_value(*edge01_));
