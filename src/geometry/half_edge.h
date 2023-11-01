449cc6ad661e4f196d874a1600800ceced7a646e
Matthew Rister <matthewrister@outlook.com>
2023-11-01 14:43:11 -0700

    fix: use correct equality operator

--- src/geometry/half_edge.h
+++ src/geometry/half_edge.h
@@ -63,7 +63,7 @@ public:
     // verify face belongs to this half-edge
     const std::array face_vertices{*face->v0(), *face->v1(), *face->v2()};
     const auto iterator = std::ranges::find_if(face_vertices, [this, edge_vertex = *vertex()](const auto& face_vertex) {
-      if (edge_vertex == face_vertex) {
+      if (*edge_vertex == *face_vertex) {
         auto edge = face_vertex.edge();
         do {
           if (*this == *edge) return true;
