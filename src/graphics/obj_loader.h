95d786da3798f5deb063826a0c772c35ce0c5e1d
Matthew Rister <matthewrister@outlook.com>
2025-11-08 20:54:53 -0800

    docs: update obj_loader documentation

--- src/graphics/obj_loader.h
+++ src/graphics/obj_loader.h
@@ -11,11 +11,11 @@ namespace obj_loader {
  * @brief Loads a triangle mesh from an .obj file.
  * @param filepath The path to the .obj file.
  * @return A mesh defined by the position, texture coordinates, normals, and indices specified in the .obj file.
- * @note At this time, only a subset of .obj file specification is supported. Specifically, only 3D vertex
- *       position, 2D texture coordinates, and 3D normals are supported. Face elements are supported and may
- *       optionally contain texture coordinate and normal indices.
  * @throw std::invalid_argument Thrown if the file format is unsupported.
  * @throw std::runtime_error Thrown if the file cannot be opened.
+ * @note At this time, only a subset of the .obj file specification is supported which includes 3D vertex positions,
+ *       2D texture coordinates, and 3D normals. Face elements are supported and may optionally contain texture
+ *       coordinates, normals, and indices.
  * @see https://en.wikipedia.org/wiki/Wavefront_.obj_file
  */
 Mesh LoadMesh(const std::filesystem::path& filepath);
